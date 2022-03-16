ae1ebe586590152c2441beae5de10a8f8f613dbf
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-16 12:05:25 +0100

    cleaned up

--- routes/myAccount.js
+++ routes/myAccount.js
@@ -1,11 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const User = require("./../Schema/Users");
 const auth = require("./../middleware/auth");
 
 //Private | Account | /api/myaccount
