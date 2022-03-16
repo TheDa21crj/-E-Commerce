09e338e061026659cddfdc9dcffa2d7fcde9b011
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-16 12:10:30 +0100

    Update TrackOrder.js

--- routes/TrackOrder.js
+++ routes/TrackOrder.js
@@ -4,9 +4,9 @@ const auth = require("./../middleware/auth");
 
 //Private | MY orders | /api/myrders
 router.get("/", auth, (req, res) => {
-    res.status(200).send({ message: `MY Orders` });
-    console.log(`req.user`);
-    console.log(req.user);
+    res.status(200).send(req.user);
+    // console.log(`req.user`);
+    // console.log(req.user);
 });
 
 module.exports = router;
