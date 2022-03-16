e9463a324bea3f61c4ba6689a5c19c7a5baab433
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-16 12:10:38 +0100

    cleaned up

--- routes/TrackOrder.js
+++ routes/TrackOrder.js
@@ -5,8 +5,6 @@ const auth = require("./../middleware/auth");
 //Private | MY orders | /api/myrders
 router.get("/", auth, (req, res) => {
     res.status(200).send(req.user);
-    // console.log(`req.user`);
-    // console.log(req.user);
 });
 
 module.exports = router;
