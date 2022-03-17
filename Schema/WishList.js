03b260d937e95b9f8b031882a8bb20890c949fcd
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-17 09:00:36 +0100

    Update WishList.js

---
+++ Schema/WishList.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const WishListSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+    },
+    WishList: [{
+        wishlist: {
+            type: "string",
+        },
+        date: {
+            type: Date,
+            default: Date.now,
+        },
+    }, ],
+});
+
+module.exports = WishList = mongoose.model("WishList", WishListSchema);
