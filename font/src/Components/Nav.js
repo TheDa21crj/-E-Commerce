3dd1f2d057cb69977aa34bd8e98483e65c2ed0e3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-19 16:26:41 +0100

    setting up

--- font/src/Components/Nav.js
+++ font/src/Components/Nav.js
@@ -1,5 +1,11 @@
 import React from "react";
+// import AbcIcon from "@mui/icons-material/Abc";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div>
+      Nav
+      {/* <AbcIcon /> */}
+    </div>
+  );
 }
