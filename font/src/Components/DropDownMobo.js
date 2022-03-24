1afa421914dd00014ad5ba90b7194df169a5aed6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-24 22:43:13 +0530

    mDiv

--- font/src/Components/DropDownMobo.js
+++ font/src/Components/DropDownMobo.js
@@ -2,5 +2,5 @@ import React from "react";
 import DbmCss from "./Css/DropDownMobo.module.css";
 
 export default function DropDownMobo() {
-  return <div>DropDownMobo</div>;
+  return <div className={DbmCss.mDiv}>DropDownMobo</div>;
 }
