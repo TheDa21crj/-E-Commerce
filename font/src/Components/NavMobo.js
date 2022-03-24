67489a2d5173c1207c3aad73cd106c7be90645b3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-24 11:34:56 +0530

    setting up

--- font/src/Components/NavMobo.js
+++ font/src/Components/NavMobo.js
@@ -1,5 +1,18 @@
 import React from "react";
+import NCss from "./Css/NavMobo.module.css";
 
 export default function NavMobo() {
-  return <div>NavMobo</div>;
+  return (
+    <div>
+      <div className={NCss.HamBurger}>
+        <div className={NCss.burger1}></div>
+        <div className={NCss.burger2}></div>
+        <div className={NCss.burger3}></div>
+      </div>
+      <div>The Da</div>
+      <div>
+        <div>Account</div>
+      </div>
+    </div>
+  );
 }
