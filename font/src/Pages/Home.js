68fd6eef356c4e51ac01f3399eff261f7239a43c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-24 11:31:19 +0530

    Update Home.js

--- font/src/Pages/Home.js
+++ font/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./../Components/Nav";
+import NavMobo from "./../Components/NavMobo";
 import Header from "./../Components/Home/Header";
 
 export default function Home() {
   return (
     <div className="BodyDiv">
       <Nav />
+      <NavMobo />
       <Header />
       Hello world
     </div>
