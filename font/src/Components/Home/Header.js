4f68728bb4d4967bc04f98deed25c6ad8f165d2f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-24 09:24:07 +0530

    react-slick added

--- font/src/Components/Home/Header.js
+++ font/src/Components/Home/Header.js
@@ -2,9 +2,33 @@ import React from "react";
 import Sliders from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SlideText from "./SlideText";
+// import SlideText from "./SlideText";
 import HeaderCss from "./Css/Header.module.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  var settings = {
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return;
+  <div className="SlideShow">
+    <div className="slides">
+      <Sliders {...settings} className="Hslide">
+        <div className="slide s1">
+          <img src={img1} />
+        </div>
+        <div className="slide ">
+          <img src={img2} />
+        </div>
+        <div className="slide">
+          <img src={img3} />
+        </div>
+      </Sliders>
+    </div>
+  </div>;
 }
