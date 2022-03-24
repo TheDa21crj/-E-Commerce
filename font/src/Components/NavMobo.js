6fd4a6728fa062c116f2310121c81fab5d1a2c21
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-24 22:30:30 +0530

    centering

--- font/src/Components/NavMobo.js
+++ font/src/Components/NavMobo.js
@@ -5,17 +5,19 @@ import PersonIcon from "@mui/icons-material/Person";
 export default function NavMobo() {
   return (
     <div className={NCss.mDiv}>
-      <div className={NCss.HamBurger}>
-        <div className={NCss.burger1}></div>
-        <div className={NCss.burger2}></div>
-        <div className={NCss.burger3}></div>
-      </div>
-      <div>
-        <h1 className={NCss.h1}>The Da</h1>
-      </div>
-      <div>
+      <div className={NCss.ChilDiv}>
+        <div className={NCss.HamBurger}>
+          <div className={NCss.burger1}></div>
+          <div className={NCss.burger2}></div>
+          <div className={NCss.burger3}></div>
+        </div>
         <div>
-          <PersonIcon fontSize="large" />
+          <h1 className={NCss.h1}>The Da</h1>
+        </div>
+        <div>
+          <div>
+            <PersonIcon fontSize="large" />
+          </div>
         </div>
       </div>
     </div>
