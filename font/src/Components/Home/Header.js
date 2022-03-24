d1bfa454dc74d6dc84925184eee0130122bd0aea
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-24 09:05:36 +0530

    i

--- font/src/Components/Home/Header.js
+++ font/src/Components/Home/Header.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Sliders from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import SlideText from "./SlideText";
 import HeaderCss from "./Css/Header.module.css";
 
 export default function Header() {
