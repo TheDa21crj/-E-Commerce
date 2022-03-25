ebcc1fc5e54e08403d098e959c038ff38f885c45
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-25 12:23:48 +0530

    men btn working

--- font/src/Components/DropDownMobo.js
+++ font/src/Components/DropDownMobo.js
@@ -3,12 +3,37 @@ import MoBoDrop from "./MoBoDrop";
 import DbmCss from "./Css/DropDownMobo.module.css";
 
 export default function DropDownMobo(props) {
+  const [showMen, setMen] = useState(false);
   return (
     <div className={DbmCss.mDiv}>
       <ul>
         <li>
-          <p>Men</p>
-          <MoBoDrop />
+          <p onClick={() => setMen(!showMen)}>Men</p>
+          {showMen ? (
+            <MoBoDrop
+              li1="T-Shirts"
+              li2="Co-ord Sets New"
+              li3="Drop-Cut T-Shirts"
+              li4="Full Sleeve T-Shirts"
+              li5="Oversized T-Shirts New"
+              li6="Polos"
+              li7="Shirts"
+              li8="Tank Tops & Vests"
+              li9="Hoodies & Sweatshirts"
+              li10="Jackets"
+              li11="Sweaters"
+              li12="Cotton Pants New"
+              li13="Joggers"
+              li14="Shorts"
+              li15="Pajamas"
+              li16="Boxers"
+              li17="Cotton Masks"
+              li18="Socks"
+              li19="Backpacks"
+            />
+          ) : (
+            ""
+          )}
         </li>
         <li>Women</li>
         <li>Kids</li>
