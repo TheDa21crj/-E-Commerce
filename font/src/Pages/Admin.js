7f73199416caafdae8f0a40bc5293b50d1130ba1
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-29 08:05:48 +0530

    i AdminC

--- font/src/Pages/Admin.js
+++ font/src/Pages/Admin.js
@@ -2,5 +2,9 @@ import React from "react";
 import AdminC from "./../Components/Admin/Admin";
 
 export default function Admin() {
-    return <div > AdminC < /div>;
+  return (
+    <div>
+      <AdminC />
+    </div>
+  );
 }
