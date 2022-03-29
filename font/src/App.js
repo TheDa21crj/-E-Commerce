91bf9f2d4427e95f9ede764b7a79727c54b9d2d8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-29 08:02:46 +0530

    Update App.js

--- font/src/App.js
+++ font/src/App.js
@@ -1,18 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
+import Admin from "./Pages/Admin";
 
 import Layout from "./Pages/Layout";
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Layout>
-  );
+    return ( <
+        Layout >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/admin"
+        element = { < Admin / > }
+        /> <
+        Route path = "*"
+        element = { < Error / > }
+        /> <
+        /Routes> <
+        /Layout>
+    );
 }
 
 export default App;
