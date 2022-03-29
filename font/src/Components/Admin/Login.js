fbcfc794c7010048fbd60338798e6bd3cfc13310
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-29 08:26:25 +0530

    html added (jsx)

--- font/src/Components/Admin/Login.js
+++ font/src/Components/Admin/Login.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="" id="" placeholder="username" />
+        <input type="password" name="" id="" placeholder="password" />
+      </form>
+      <button>Login</button>
+    </div>
+  );
 }
