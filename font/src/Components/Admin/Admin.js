fc1366087d8b10ec312826fc180f42b36de7804b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-29 08:07:13 +0530

    Update Admin.js

--- font/src/Components/Admin/Admin.js
+++ font/src/Components/Admin/Admin.js
@@ -1,11 +1,5 @@
 import React from "react";
-import Nav from "./../Nav";
 
 export default function Admin() {
-  return (
-    <div>
-      <Nav />
-      Admin
-    </div>
-  );
+  return <div>Admin</div>;
 }
