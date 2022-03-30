841bdf285e06188681e7875e1ca8a7fe66183bd3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-30 11:42:44 +0530

    Update Login.js

--- font/src/Components/Admin/Login.js
+++ font/src/Components/Admin/Login.js
@@ -2,23 +2,13 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 
 export default function Login() {
-    return ( <
-        div >
-        <
-        form action = "" >
-        <
-        input type = "text"
-        name = ""
-        id = ""
-        placeholder = "username" / >
-        <
-        input type = "password"
-        name = ""
-        id = ""
-        placeholder = "password" / >
-        <
-        /form> <
-        button > Login < /button> <
-        /div>
-    );
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="" id="" placeholder="username" />
+        <input type="password" name="" id="" placeholder="password" />
+      </form>
+      <button> Login </button>
+    </div>
+  );
 }
