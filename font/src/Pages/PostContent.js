9f1bf8c4b85d83d5f92866d78ad85a65392980be
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-30 13:02:59 +0530

    Update PostContent.js

--- font/src/Pages/PostContent.js
+++ font/src/Pages/PostContent.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function PostContent() {
+  useEffect(() => {
+    verify();
+  }, []);
+
+  const verify = async () => {
+    try {
+      const res = fetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>PostContent</div>;
 }
