0c0758f10e2470b4680f30c43dd0fa11b6616c53
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-02 21:17:20 +0530

    adding routes

--- font/src/App.js
+++ font/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+import Admin from "./Pages/Admin";
+import Layout from "./Pages/Layout";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
+        {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
