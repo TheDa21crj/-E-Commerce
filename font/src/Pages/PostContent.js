64b27d05d0156f9503e8e57fa82b0aee1d2457aa
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-02 09:07:07 +0530

    cleaning up

--- font/src/Pages/PostContent.js
+++ font/src/Pages/PostContent.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
 
 export default function PostContent() {
-  useEffect(() => {
-    verify();
-  }, []);
+  // useEffect(() => {
+  //   verify();
+  // }, []);
 
-  const verify = async () => {
-    try {
-      const res = fetch();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const verify = async () => {
+  // try {
+  // const res = fetch();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  return <div>PostContent</div>;
+  return <div> PostContent </div>;
 }
