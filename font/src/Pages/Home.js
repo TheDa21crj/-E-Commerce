5672bb676a0636662d8d7574eb636b7353da2084
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 16:12:56 +0530

    Update Home.js

--- font/src/Pages/Home.js
+++ font/src/Pages/Home.js
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      Home
-      <Link to="/admin/login">Admin</Link>
+      Home <Link to="/admin/login"> Admin </Link>
     </div>
   );
 }
