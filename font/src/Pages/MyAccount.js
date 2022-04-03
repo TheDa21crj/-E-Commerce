ea9fbd040027050ecd96d3c05f7aae20376aad90
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 17:14:52 +0530

    Create MyAccount.js

---
+++ font/src/Pages/MyAccount.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./../Components/Nav";
+import NavMobo from "./../Components/NavMobo";
+import Header from "./../Components/Home/Header";
+import Footer from "./../Components/Footer";
+
+export default function MyAccount() {
+  return (
+    <div>
+      <Nav />
+      <NavMobo />
+      MyAccount
+      <Footer />
+    </div>
+  );
+}
