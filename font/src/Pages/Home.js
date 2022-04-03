a95946fcfca65103a1fe3b8c9f8060b3c92cbd9e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 16:43:14 +0530

    setting up front end

--- font/src/Pages/Home.js
+++ font/src/Pages/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Nav from "./../Components/Nav";
+import NavMobo from "./../Components/NavMobo";
+import Header from "./../Components/Home/Header";
+import Footer from "./../Components/Footer";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <Nav />
-      Home <Link to="/admin/login"> Admin </Link>
+      <NavMobo />
+      <Header />
+      Hello world
+      <Footer />
+      Home <Link to="/admin/login"> Admin </Link>{" "}
     </div>
   );
 }
