ecb9765cb48a377232a07f7f907f26b9a2dd692e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 16:17:45 +0530

    working on front end

--- font/src/Pages/Home.js
+++ font/src/Pages/Home.js
@@ -1,8 +1,4 @@
 import React from "react";
-import Nav from "./../Components/Nav";
-import NavMobo from "./../Components/NavMobo";
-import Header from "./../Components/Home/Header";
-import Footer from "./../Components/Footer";
 import { Link } from "react-router-dom";
 
 export default function Home() {
