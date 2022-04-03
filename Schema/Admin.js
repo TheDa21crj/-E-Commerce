6d32a5ee548f9e239133f9af967ebb8f80063469
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 10:50:57 +0530

    Schema admin added

---
+++ Schema/Admin.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema({
+    username: {
+        type: "string",
+        required: true,
+    },
+    password: {
+        type: "string",
+        required: true,
+    },
+    tokens: [{
+        token: {
+            type: "string",
+            required: true,
+        },
+    }, ],
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = Users = mongoose.model("Admin", AdminSchema);
