4a524badee87461251e7b05ee32a50d0c33bedde
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 16:22:58 +0530

    working on nav

--- font/src/Pages/Home.js
+++ font/src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Nav from "./../Components/Nav";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       Home <Link to="/admin/login"> Admin </Link>
     </div>
   );
