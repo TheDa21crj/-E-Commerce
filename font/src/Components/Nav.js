dbac2bf27d63ee0566b3653b41e4cc50d39f1db5
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 16:37:26 +0530

    Update Nav.js

--- font/src/Components/Nav.js
+++ font/src/Components/Nav.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import PersonIcon from "@mui/icons-material/Person";
+import PersonIcon from "@mui/icons-material/Person";
 
 export default function Nav() {
   return (
     <div>
-      {/* <PersonIcon /> */}
+      <PersonIcon />
       Nav
     </div>
   );
