de296aeeb1d0e15f64c0888147392c4b55f78a15
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-03 10:39:27 +0530

    basic html added

--- font/src/Pages/AdminLogin.js
+++ font/src/Pages/AdminLogin.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function AdminLogin() {
-  return <div>AdminLogin</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="" method="POST">
+        <div>
+          <label htmlFor="username">username</label>
+          <input type="text" name="" id="" placeholder="username" />
+        </div>
+        <div>
+          <label htmlFor="username">password</label>
+          <input type="text" name="" id="" placeholder="password" />
+        </div>
+      </form>
+    </div>
+  );
 }
