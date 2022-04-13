4ab302432ddec63e29c8c3db8438b7cdea83b36d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-13 08:46:23 +0530

    Update Login.js

--- font/src/Pages/Login.js
+++ font/src/Pages/Login.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Nav from "./../Components/Nav";
+import NavMobo from "./../Components/NavMobo";
+import Footer from "./../Components/Footer";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Nav />
+      <NavMobo />
+      Login
+    </div>
+  );
 }
