51a425df015444c5ef85bc4dcd6a1cec232daa2c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-26 10:17:17 +0530

    rfc

---
+++ font/src/Pages/Membership.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Nav from "./../Components/Nav";
+import NavMobo from "./../Components/NavMobo";
+
+export default function Membership() {
+  return <div>Membership</div>;
+}
