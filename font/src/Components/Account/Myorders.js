53ae93b4717d91d3101e35708c584dc65b55111d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-27 09:50:59 +0530

    Update Myorders.js

--- font/src/Components/Account/Myorders.js
+++ font/src/Components/Account/Myorders.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Myorders() {
+export default function Myorders(props) {
   return (
     <div>
       <h1>My Orders</h1>
+      <p>{props.id}</p>
     </div>
   );
 }
