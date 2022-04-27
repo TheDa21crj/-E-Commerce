4f482f2ed38b26e23b50f8cf4f8ba93ce9cfe9b3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-27 07:01:45 +0530

    route

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.use("/api", require("./routes/User"));
 // app.use("/api/Cart", require("./routes/Cart"));
 app.use("/api/RegAdmin", require("./routes/RegAdmin"));
 app.use("/api/admin/login", require("./routes/Admin"));
+app.use("/api/admin/Products", require("./routes/Products"));
+
 app.use("/api/admin/", require("./routes/AdminPost"));
 
 const port = process.env.PORT || 5000;
