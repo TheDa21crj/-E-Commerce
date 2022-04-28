7be8368c44a92b43d32ae98c68c99469effab38c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-28 19:58:42 +0530

    Update TopSelling.js

--- font/src/Components/Home/TopSelling.js
+++ font/src/Components/Home/TopSelling.js
@@ -3,10 +3,22 @@ import TSCss from "./Css/TopSelling.module.css";
 import TopSellingJson from "./../../Data/TopSelling.json";
 
 export default function TopSelling() {
+  var settingTS = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: false,
+  };
   return (
     <div className={TSCss.mDiv}>
       <div className={TSCss.H1Div}>
         <h1 className={TSCss.h1}>Top Selling</h1>
+      </div>
+      <div>
+        <Sliders {...settingTS} className={TSCss.Hslide}></Sliders>
       </div>
     </div>
   );
