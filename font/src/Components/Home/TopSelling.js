fb0e22327716017019336c301b02b0764ad8919c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-28 22:11:17 +0530

    Update TopSelling.js

--- font/src/Components/Home/TopSelling.js
+++ font/src/Components/Home/TopSelling.js
@@ -63,7 +63,7 @@ export default function TopSelling() {
                   />
                 </div>
                 <p>{value.name}</p>
-                <p>{value.category}</p>
+                <p>₹{value.price}</p>
               </div>
             );
           })}
