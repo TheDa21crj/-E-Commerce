858a4935fb4481dfc649fecc9c698f3027a75b35
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-29 07:08:30 +0530

    Update ProductsDeatils.js

--- font/src/Pages/ProductsDeatils.js
+++ font/src/Pages/ProductsDeatils.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "./../Components/Nav";
 import NavMobo from "./../Components/NavMobo";
 import Footer from "./../Components/Footer";
 
-export default function ProductsDeatils() {
+export default function ProductsDeatils(props) {
+  useEffect(() => {
+    const id = props.params.id;
+    console.log(id);
+  });
   return (
     <div>
       <Nav />
