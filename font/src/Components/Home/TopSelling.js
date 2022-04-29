721f7ff61e36f2ea048f9129cc0f7a71e0794a1d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-29 18:40:39 +0530

    Update TopSelling.js

--- font/src/Components/Home/TopSelling.js
+++ font/src/Components/Home/TopSelling.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import TSCss from "./Css/TopSelling.module.css";
-// import TopSellingJson from "./../../Data/TopSelling.json";
 import { Link } from "react-router-dom";
 import Sliders from "react-slick";
 import "slick-carousel/slick/slick.css";
