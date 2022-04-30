6693e2f9d4b0e41a719bba1b278432e14fe21f94
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 17:33:27 +0530

    Update TableCm.js

--- font/src/Components/Product/TableCm.js
+++ font/src/Components/Product/TableCm.js
@@ -19,33 +19,33 @@ export default function TableCm() {
         </tr>
         <tr>
           <th>S</th>
-          <th>17.75</th>
-          <th>38.0</th>
-          <th>30.0</th>
+          <th>44.0</th>
+          <th>96.0</th>
+          <th>76.0</th>
         </tr>
         <tr>
           <th>M</th>
-          <th>18.00</th>
-          <th>40.0</th>
-          <th>32.0</th>
+          <th>45.5</th>
+          <th>101.0</th>
+          <th>81.0</th>
         </tr>
         <tr>
           <th>L</th>
-          <th>18.62</th>
-          <th>42.0</th>
-          <th>34.0</th>
+          <th>47.00</th>
+          <th>106.0</th>
+          <th>86.0</th>
         </tr>
         <tr>
           <th>XL</th>
-          <th>19.25</th>
-          <th>44.0</th>
-          <th>36.0</th>
+          <th>48.5</th>
+          <th>111.0</th>
+          <th>91.0</th>
         </tr>
         <tr>
           <th>XXL</th>
-          <th>19.87</th>
-          <th>46.0</th>
-          <th>38.0</th>
+          <th>50.0</th>
+          <th>116.0</th>
+          <th>96.0</th>
         </tr>
       </table>
     </div>
