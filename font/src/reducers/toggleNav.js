e7e854ef5ce4f0d0ee19312cf30f53e70f267c7e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 20:06:37 +0530

    Update toggleNav.js

--- font/src/reducers/toggleNav.js
+++ font/src/reducers/toggleNav.js
@@ -1,15 +1,14 @@
 export const initialState = "";
 
 const toggleNav = (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case "TOGGLENAV":
-      //   console.log(action);
-      state = payload;
-      return state;
-    default:
-      return state;
-  }
+    const { type, payload } = action;
+    switch (type) {
+        case "TOGGLENAV":
+            state = payload;
+            return state;
+        default:
+            return state;
+    }
 };
 
 export default toggleNav;
