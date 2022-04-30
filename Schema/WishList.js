14df7844bae0065c1aeaecd00760b4bde50cc103
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 21:50:33 +0530

    Update WishList.js

--- Schema/WishList.js
+++ Schema/WishList.js
@@ -4,7 +4,7 @@ const WishListSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true,
+        // required: true,
     },
     Product: [{
         id: {
