0e0ae0e5cf0005492ba638155c6422084b916045
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 17:22:46 +0530

    Update TableIn.js

--- font/src/Components/Product/TableIn.js
+++ font/src/Components/Product/TableIn.js
@@ -18,10 +18,34 @@ export default function Table() {
           </th>
         </tr>
         <tr>
-          <th>Size</th>
-          <th>16.75</th>
-          <th></th>
-          <th></th>
+          <th>S</th>
+          <th>17.75</th>
+          <th>38.0</th>
+          <th>30.0</th>
+        </tr>
+        <tr>
+          <th>M</th>
+          <th>18.00</th>
+          <th>40.0</th>
+          <th>32.0</th>
+        </tr>
+        <tr>
+          <th>L</th>
+          <th>18.62</th>
+          <th>42.0</th>
+          <th>34.0</th>
+        </tr>
+        <tr>
+          <th>XL</th>
+          <th>19.25</th>
+          <th>44.0</th>
+          <th>36.0</th>
+        </tr>
+        <tr>
+          <th>XXL</th>
+          <th>19.87</th>
+          <th>46.0</th>
+          <th>38.0</th>
         </tr>
       </table>
     </div>
