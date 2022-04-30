d685990449b4422e05cb692f7b26d4b8cd2cb668
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 17:14:45 +0530

    Update TableIn.js

--- font/src/Components/Product/TableIn.js
+++ font/src/Components/Product/TableIn.js
@@ -2,5 +2,28 @@ import React from "react";
 import TCss from "./CSS/TableIn.module.css";
 
 export default function Table() {
-  return <div>Table</div>;
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>SIZE</th>
+          <th>
+            <p>A. TO FIT SHOULDER</p>
+          </th>
+          <th>
+            <p>B. TO FIT CHEST</p>
+          </th>
+          <th>
+            <p>C. TO FIT WAIST</p>
+          </th>
+        </tr>
+        <tr>
+          <th>Size</th>
+          <th>16.75</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </table>
+    </div>
+  );
 }
