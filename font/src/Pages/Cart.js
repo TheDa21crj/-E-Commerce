bbb59bb0d16b6073a485eddfd552aa81d61b2532
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 20:58:20 +0530

    Update Cart.js

--- font/src/Pages/Cart.js
+++ font/src/Pages/Cart.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Nav from "./../Components/Nav";
+import NavMobo from "./../Components/NavMobo";
+import Footer from "./../Components/Footer";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <Nav />
+      <NavMobo />
+      Cart
+      <Footer />
+    </div>
+  );
 }
