d1b89f0a4acf05fe11a40fb67223366d709566bc
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-30 16:19:19 +0530

    Update SizeChart.js

--- font/src/Components/Product/SizeChart.js
+++ font/src/Components/Product/SizeChart.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 import SCCss from "./CSS/SizeChart.module.css";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function SizeChart(props) {
-  // const [showPOP, hidePOP] = useState(props.state);
+  const close = useRef();
+
+  useEffect(() => {
+    document.addEventListener("click", handleClose);
+    return () => document.removeEventListener("click", handleClose);
+  }, []);
+
+  const handleClose = (e) => {
+    console.log("close");
+  };
 
   return (
     <div className={SCCss.mDiv}>
-      <div className={SCCss.mDivInner} onClick={() => console.log("Close")}>
+      <div className={SCCss.mDivInner} ref={close}>
         <div className={SCCss.ConDiv}>
           {props.name}
           <CloseIcon fontSize="large" onClick={() => props.state(false)} />
