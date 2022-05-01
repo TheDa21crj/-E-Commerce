cfda3bb0a07df4da055439764c5646888eb844db
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-01 09:47:49 +0530

    Update Cart.js

--- font/src/Pages/Cart.js
+++ font/src/Pages/Cart.js
@@ -1,10 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./../Components/Nav";
 import NavMobo from "./../Components/NavMobo";
 import Footer from "./../Components/Footer";
 
 export default function Cart() {
-  const CartCheck = async () => {};
+  const CartCheck = async () => {
+    try {
+      let quantity = showSelect;
+      const res = await fetch("/api/Wishlist", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (data.errors) {
+        return;
+      }
+      if (data) {
+        return console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+      return navigate("/login");
+    }
+  };
 
   useEffect(() => {
     CartCheck();
