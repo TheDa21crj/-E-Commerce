e26ce8ce1e65813b9bfdb8f5690965894cb7dac0
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-03 07:48:26 +0530

    Update Membership.js

--- font/src/Pages/Membership.js
+++ font/src/Pages/Membership.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "./../Components/Nav";
 import NavMobo from "./../Components/NavMobo";
 
 export default function Membership() {
+  useEffect(() => {
+    document.title =
+      "Join The Da Store Exclusive Membership & Shop At Discounted Prices";
+  });
   return (
     <div>
       <Nav />
