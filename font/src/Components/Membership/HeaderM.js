424dfbd713530522d5c03bd4eb5dca7821611f2c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-03 09:06:24 +0530

    Update HeaderM.js

--- font/src/Components/Membership/HeaderM.js
+++ font/src/Components/Membership/HeaderM.js
@@ -3,6 +3,7 @@ import img from "./../../Img/clothes.jpg";
 import Sliders from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import HMCss from "./CSS/Header.module.css";
 
 export default function HeaderM() {
   var settingM = {
@@ -14,5 +15,42 @@ export default function HeaderM() {
     slidesToScroll: 1,
     autoplay: true,
   };
-  return <div>HeaderM</div>;
+  return (
+    <div>
+      <div className={HMCss.HeaderDiv}>
+        <img src={img} alt="" className={HMCss.imgTag} />
+        <div className={HMCss.headerOverlayDiv}>
+          <div>
+            <p className={HMCss.SAVEBIG}>SAVE BIG</p>
+            <div className={HMCss.onAllDiv}>
+              <p className={HMCss.ONALLPRODUCTS}>ON ALL PRODUCTS. EVERY DAY.</p>
+            </div>
+          </div>
+          <div className={HMCss.FunFactsDiv}>
+            <p className={HMCss.FunFactsPTag}>Fun Facts</p>
+          </div>
+          <div className={HMCss.slideDiv}>
+            <Sliders {...settingM} className={HMCss.Hslide}>
+              <div>
+                <p>
+                  Add the membership to your cart
+                  <br />
+                  and get the discount on this order itself.
+                </p>
+              </div>
+              <div>
+                <p>Most people save the membership fee on their first order.</p>
+              </div>
+              <div>
+                <p>The average exclusive member SAVES Rs. 2600/year.</p>
+              </div>
+              <div>
+                <p>Most people become members and don’t pay full price.</p>
+              </div>
+            </Sliders>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
