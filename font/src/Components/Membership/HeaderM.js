6c02ed46fee8383862b099bcac3c50ce23ce201e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-03 08:59:59 +0530

    rfc

---
+++ font/src/Components/Membership/HeaderM.js
@@ -0,0 +1,9 @@
+import React from "react";
+import img from "./../../Img/clothes.jpg";
+import Sliders from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function HeaderM() {
+  return <div>HeaderM</div>;
+}
