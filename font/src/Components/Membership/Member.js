589d816e8928e93510752e8131cd59948b6a109b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 12:07:39 +0530

    Update Member.js

--- font/src/Components/Membership/Member.js
+++ font/src/Components/Membership/Member.js
@@ -1,5 +1,10 @@
 import React from "react";
+import MemCss from "./CSS/Member.module.css";
 
 export default function Member() {
-  return <div>BECOME AN EXCLUSIVE MEMBER</div>;
+  return (
+    <div className={MemCss.mDiv}>
+      <p className={MemCss.titlePtag}>Become An Exclusive Member</p>
+    </div>
+  );
 }
