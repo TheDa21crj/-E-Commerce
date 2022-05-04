418d7a1f5df46d9ce1010bf7aaf549994ec401f2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 12:38:15 +0530

    Update Member.js

--- font/src/Components/Membership/Member.js
+++ font/src/Components/Membership/Member.js
@@ -7,9 +7,9 @@ export default function Member() {
     <div className={MemCss.mDiv}>
       <p className={MemCss.titlePtag}>Become An Exclusive Member</p>
       <div className={MemCss.CarsPricemDiv}>
-        <PriceCards />
-        <PriceCards />
-        <PriceCards />
+        <PriceCards monthNum="12" monthPrice="25" popular="Most Popular" />
+        <PriceCards monthNum="3" monthPrice="83" />
+        <PriceCards monthNum="1" monthPrice="199" />
       </div>
     </div>
   );
