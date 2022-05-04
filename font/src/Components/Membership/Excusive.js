f4ea33a163826e9e8811c62ee355143f4e7859d0
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 10:25:21 +0530

    Update Excusive.js

--- font/src/Components/Membership/Excusive.js
+++ font/src/Components/Membership/Excusive.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Ecss from "./CSS/Excusive.module.css";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 export default function Excusive() {
-  return <div>Exclusive Member Benefits</div>;
+  return (
+    <div>
+      <div className={Ecss.titlemDiv}>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
 }
