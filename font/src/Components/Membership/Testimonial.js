2d65a111d788708a553ec549fa74fac7839ae9aa
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 20:36:25 +0530

    Update Testimonial.js

--- font/src/Components/Membership/Testimonial.js
+++ font/src/Components/Membership/Testimonial.js
@@ -6,22 +6,50 @@ import "slick-carousel/slick/slick-theme.css";
 
 export default function Testimonial() {
   var settingT = {
-    dots: false,
+    dots: true,
     infinite: true,
-    arrows: true,
+    arrows: false,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
   };
   return (
     <div className={TeCss.mDiv}>
       <p className={TeCss.TestimonialtitlePTag}>Testimonial</p>
       <div>
-        <Sliders {...settingT} className={NACss.Hslide}>
-          <div></div>
-          <div></div>
-          <div></div>
+        <Sliders {...settingT} className={TeCss.Hslide}>
+          <div>
+            <div>
+              <p>
+                The exclusive membership is great, it helps save so much money.
+                You not only recover the money you've spent on it, but save so
+                much more!
+              </p>
+              <p>Atharva, Pune</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>
+                The exclusive membership has definitely been worth the value.
+                I've never had a bad experience with the membership and all
+                these continuous discounts are very beneficial to the customer.
+              </p>
+              <p>Bhuvandeep, Bangalore</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>
+                The exclusive membership has been a real blessing, it gives you
+                first preference for limited edition clothes, it also helps you
+                save so much and it barely costs anything in comparison with
+                what you save.
+              </p>
+              <p>Rahul, Pune</p>
+            </div>
+          </div>
         </Sliders>
       </div>
     </div>
