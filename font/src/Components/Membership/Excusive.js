49926210d45638c77da7ff324c0cac7c34aca300
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 10:53:05 +0530

    Update Excusive.js

--- font/src/Components/Membership/Excusive.js
+++ font/src/Components/Membership/Excusive.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Ecss from "./CSS/Excusive.module.css";
+import Benifits from "./Benifits";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 export default function Excusive() {
@@ -16,6 +17,30 @@ export default function Excusive() {
               className={Ecss.EmojiEventsIcon}
             />
           </p>
+        </div>
+      </div>
+      <div className={Ecss.BenifitsRowDiv}>
+        <div className={Ecss.BenifitsDiv}>
+          <div>
+            <Benifits
+              title="BIG DISCOUNTS"
+              content="Save big on ALL products every day. Why wait for a sale?"
+            />
+            <Benifits
+              title="EARLY ACCESS"
+              content="Get your hands on select products and designs before others do."
+            />
+          </div>
+          <div>
+            <Benifits
+              title="FREE DELIVERY"
+              content="Our shipping charges are on us."
+            />
+            <Benifits
+              title="PRIORITISED SHIPPING"
+              content="We ship your orders before everyone else’s."
+            />
+          </div>
         </div>
       </div>
     </div>
