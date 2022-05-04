741e09af5cb78d97d216e69c8891b5319b1fd61c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 20:33:08 +0530

    slider

--- font/src/Components/Membership/Testimonial.js
+++ font/src/Components/Membership/Testimonial.js
@@ -5,20 +5,24 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function Testimonial() {
+  var settingT = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+  };
   return (
     <div className={TeCss.mDiv}>
       <p className={TeCss.TestimonialtitlePTag}>Testimonial</p>
-
       <div>
-        slider
-        <br />
-        t1
-        <br />
-        t2
-        <br />
-        t3
-        <br />
-        ...
+        <Sliders {...settingT} className={NACss.Hslide}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </Sliders>
       </div>
     </div>
   );
