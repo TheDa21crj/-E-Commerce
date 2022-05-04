8d10017f25af64c8557466a7c83e62f1d2820dfb
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 12:17:55 +0530

    Update Member.js

--- font/src/Components/Membership/Member.js
+++ font/src/Components/Membership/Member.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PriceCards from "./PriceCards";
 import MemCss from "./CSS/Member.module.css";
 
 export default function Member() {
   return (
     <div className={MemCss.mDiv}>
       <p className={MemCss.titlePtag}>Become An Exclusive Member</p>
+      <div className={MemCss.CarsPricemDiv}>
+        <PriceCards />
+        <PriceCards />
+        <PriceCards />
+      </div>
     </div>
   );
 }
