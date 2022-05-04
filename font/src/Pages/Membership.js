566d3959188b34efcf4b8e56432152eab2528df4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 14:09:57 +0530

    i

--- font/src/Pages/Membership.js
+++ font/src/Pages/Membership.js
@@ -6,6 +6,7 @@ import Footer from "./../Components/Footer";
 import HeaderM from "./../Components/Membership/HeaderM";
 import Excusive from "./../Components/Membership/Excusive";
 import Member from "./../Components/Membership/Member";
+import Testimonial from "./../Components/Membership/Testimonial";
 
 export default function Membership() {
   useEffect(() => {
@@ -21,8 +22,7 @@ export default function Membership() {
       <div>
         <Excusive />
         <Member />
-        <br />
-        TESTIMONIALS
+        <Testimonial />
       </div>
       <Footer />
     </div>
