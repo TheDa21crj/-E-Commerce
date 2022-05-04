460e7e500aa482227bb0aa733cdbff0757197d19
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 20:30:22 +0530

    import

--- font/src/Components/Membership/Testimonial.js
+++ font/src/Components/Membership/Testimonial.js
@@ -1,5 +1,8 @@
 import React from "react";
 import TeCss from "./CSS/Testimonial.module.css";
+import Sliders from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Testimonial() {
   return (
