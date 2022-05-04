9810c8ba052ba5692eacf9fbdfc490cad9608c5e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 12:43:37 +0530

    Update PriceCards.js

--- font/src/Components/Membership/PriceCards.js
+++ font/src/Components/Membership/PriceCards.js
@@ -3,9 +3,16 @@ import PCss from "./CSS/PriceCards.module.css";
 
 export default function PriceCards(props) {
   const [show, set] = useState(props.popular);
+
   return (
     <div className={PCss.mDiv}>
-      <p className={PCss.Mostpopular}>{props.popular}</p>
+      {show ? (
+        <div className={PCss.popularDiv}>
+          <p className={PCss.Mostpopular}>{props.popular}</p>
+        </div>
+      ) : (
+        <div className={PCss.popularDiv}></div>
+      )}
 
       <p>{props.monthNum} Months</p>
       <p>₹ {props.monthPrice}/Month</p>
