90fc89a51bd566ce7476d75364f18e5e5fbdae04
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 14:11:10 +0530

    Update Testimonial.js

--- font/src/Components/Membership/Testimonial.js
+++ font/src/Components/Membership/Testimonial.js
@@ -2,5 +2,9 @@ import React from "react";
 import TeCss from "./CSS/Testimonial.module.css";
 
 export default function Testimonial() {
-  return <div>Testimonial</div>;
+  return (
+    <div className={TeCss.mDiv}>
+      <p className={TeCss.TestimonialtitlePTag}>Testimonial</p>
+    </div>
+  );
 }
