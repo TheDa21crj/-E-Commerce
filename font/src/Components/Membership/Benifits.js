eeb90c996afc95776dc6dbab5e7ce4bc054d17f8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 11:47:05 +0530

    Update Benifits.js

--- font/src/Components/Membership/Benifits.js
+++ font/src/Components/Membership/Benifits.js
@@ -4,16 +4,19 @@ import BenifitsCss from "./CSS/Benifits.module.css";
 export default function Benifits(props) {
   return (
     <div className={BenifitsCss.mDiv}>
+      <div className={BenifitsCss.imgDiv}>{props.img}</div>
       <div>
-        <p className={BenifitsCss.titlePTag}>{props.title}</p>
-      </div>
-      <div className={BenifitsCss.LineDiv}>
-        <div className={BenifitsCss.CircleDiv}></div>
-      </div>
-      <div className={BenifitsCss.contentDiv}>
-        <p className={BenifitsCss.contentDivPTag}>
-          <i>{props.content}</i>
-        </p>
+        <div>
+          <p className={BenifitsCss.titlePTag}>{props.title}</p>
+        </div>
+        <div className={BenifitsCss.LineDiv}>
+          <div className={BenifitsCss.CircleDiv}></div>
+        </div>
+        <div className={BenifitsCss.contentDiv}>
+          <p className={BenifitsCss.contentDivPTag}>
+            <i>{props.content}</i>
+          </p>
+        </div>
       </div>
     </div>
   );
