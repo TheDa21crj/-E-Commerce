001a4eaf6e0c5308f20e260df572216d89d5adf9
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-04 13:51:47 +0530

    Update Member.js

--- font/src/Components/Membership/Member.js
+++ font/src/Components/Membership/Member.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import PriceCards from "./PriceCards";
 import MemCss from "./CSS/Member.module.css";
 
 export default function Member() {
+  const [showSelect1, setSelect1] = useState(true);
   return (
-    <div className={MemCss.mDiv} id={MemCss.notSelected}>
+    <div className={MemCss.mDiv}>
       <p className={MemCss.titlePtag}>Become An Exclusive Member</p>
       <div className={MemCss.CarsPricemDiv}>
         <PriceCards
@@ -13,6 +14,7 @@ export default function Member() {
           popular="Most Popular"
           price="299"
           percentage="87.50"
+          selected={showSelect1}
         />
         <PriceCards monthNum="3" monthPrice="83" price="249" percentage="58" />
         <PriceCards monthNum="1" monthPrice="199" price="199" />
