de20c7abb6ee545cb92342873d04f6c238de3d3c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-05 13:01:16 +0530

    react

---
+++ font/src/Pages/Home.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    document.title = "Online Shopping for Men & Women: The Da";
+  });
+  return (
+    <div>
+      <br />
+      CATEGORIES
+      <br />
+      Customer reviews
+      <br />
+      OFFICIAL MERCHANDISE/Brands
+      <br />
+      <br />
+      Hello world
+    </div>
+  );
+}
