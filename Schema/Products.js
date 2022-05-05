05d7b263209c9e5686b058e64ef05eff393a7958
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-05 12:47:28 +0530

    initial commit

---
+++ Schema/Products.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: "string",
+        required: true,
+    },
+    imageSrc: {
+        type: "string",
+        required: true,
+    },
+    des: {
+        type: "string",
+        required: true,
+    },
+    rating: {
+        type: "Number",
+        required: true,
+        default: 0,
+    },
+    NumReview: {
+        type: "Number",
+        required: true,
+        default: 0,
+    },
+    price: {
+        type: "Number",
+        required: true,
+        default: 0,
+    },
+    gender: {
+        type: "string",
+        required: true,
+    },
+    category: {
+        type: "string",
+        default: "null",
+        // required: true,
+    },
+    sold: {
+        type: "Number",
+        default: 0,
+        // required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = Users = mongoose.model("Product", ProductSchema);
