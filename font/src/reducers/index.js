f1e1fb47968fd13b434724aff780be474896a2ce
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-05 08:14:43 +0530

    rootReducer

--- font/src/reducers/index.js
+++ font/src/reducers/index.js
@@ -1,8 +1,10 @@
 import toggleNav from "./toggleNav";
+import incWish from "./addwish";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     toggleNav,
+    incWish,
 });
 
 export default rootReducer;
