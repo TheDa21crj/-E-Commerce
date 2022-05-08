75ace29a42298f9e6b8d7c0fc805668ada2d4a68
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-08 17:01:08 +0530

    Update Nav.js

---
+++ font/src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavDes from "./NavDes";
+import NavMobo from "./NavMobo";
+
+export default function Nav() {
+  return (
+    <div>
+      <NavDes />
+      <NavMobo />
+    </div>
+  );
+}
