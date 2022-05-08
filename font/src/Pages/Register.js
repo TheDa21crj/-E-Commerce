34c4f67581abc77c75be8b5db74e40845d3a5aa7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-08 21:38:26 +0530

    Update Register.js

--- font/src/Pages/Register.js
+++ font/src/Pages/Register.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import Nav from "./../Components/Nav";
 import Footer from "./../Components/Footer";
 import { Link } from "react-router-dom";
-import RegCss from "./Css/Register.module.css";
+import LoginCss from "./Css/Login.module.css";
 import { useNavigate } from "react-router-dom";
+import img from "./../Img/sign.png";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export default function Register() {
   const [showUser, setUser] = useState({ email: "", password: "" });
+  const [showPassword, setPassword] = useState(false);
 
   const navigate = useNavigate();
 
@@ -48,29 +52,48 @@ export default function Register() {
   return (
     <div>
       <Nav />
-      Register
-      <form action="" method="POST">
-        <input
-          type="email"
-          name="email"
-          id=""
-          placeholder="Email"
-          value={showUser.email}
-          onChange={DataInp}
-        />
-        <input
-          type="password"
-          name="password"
-          id=""
-          placeholder="Password"
-          value={showUser.password}
-          onChange={DataInp}
-        />
-      </form>
-      <button onClick={PostData}>Register</button>
-      <br />
-      <Link to="/login">Login</Link>
-      <br />
+      <div className={LoginCss.contentMDiv}>
+        <div className={LoginCss.Div}>
+          <div className={LoginCss.LeftDiv}>
+            <p className={LoginCss.LoginPTag}>Register</p>
+            <form action="" method="POST" className={LoginCss.LoginFormTag}>
+              <input
+                type="email"
+                name="email"
+                id={LoginCss.email}
+                placeholder="Email"
+                value={showUser.email}
+                onChange={DataInp}
+              />
+              <div className={LoginCss.passwordDiv}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id={LoginCss.password}
+                  placeholder="Enter Your Password"
+                  value={showUser.password}
+                  onChange={DataInp}
+                />
+                <div
+                  id={LoginCss.ShowPasswordDiv}
+                  onClick={() => {
+                    setPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? <RemoveRedEyeIcon /> : <VisibilityOffIcon />}
+                </div>
+              </div>
+            </form>
+            <button onClick={PostData}>Register</button>
+            <br />
+            <Link to="/login">Login</Link>
+          </div>
+          <div className={LoginCss.RightDiv}>
+            <img src={img} alt="" className={LoginCss.ImgLogin} />
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
