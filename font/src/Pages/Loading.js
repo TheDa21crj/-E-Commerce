803a69ce43bdf59b4578889c7f663559478ebf2e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-10 11:48:00 +0530

    Update Loading.js

--- font/src/Pages/Loading.js
+++ font/src/Pages/Loading.js
@@ -5,16 +5,16 @@ export default function Loading() {
   return (
     <div className={LoadCss.mDiv}>
       <div className={LoadCss.loader}>
-        <span style="--i:1;"></span>
-        <span style="--i:2;"></span>
-        <span style="--i:3;"></span>
-        <span style="--i:4;"></span>
-        <span style="--i:5;"></span>
-        <span style="--i:6;"></span>
-        <span style="--i:7;"></span>
-        <span style="--i:8;"></span>
-        <span style="--i:9;"></span>
-        <span style="--i:10;"></span>
+        <span style={{ "--i": 1 }}></span>
+        <span style={{ "--i": 2 }}></span>
+        <span style={{ "--i": 3 }}></span>
+        <span style={{ "--i": 4 }}></span>
+        <span style={{ "--i": 5 }}></span>
+        <span style={{ "--i": 6 }}></span>
+        <span style={{ "--i": 7 }}></span>
+        <span style={{ "--i": 8 }}></span>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 10 }}></span>a
       </div>
     </div>
   );
