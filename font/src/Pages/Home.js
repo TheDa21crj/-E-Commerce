15d456ec023ae0303c9d6e1f0b29ab379222dce5
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-10 13:16:52 +0530

    Update Home.js

--- font/src/Pages/Home.js
+++ font/src/Pages/Home.js
@@ -1,31 +1,50 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
 import Nav from "./../Components/Nav";
 import Header from "./../Components/Home/Header";
 import Footer from "./../Components/Footer";
 import NewArrival from "./../Components/Home/NewArrival";
 import TopSelling from "./../Components/Home/TopSelling";
+import Loading from "./Loading";
+// redux
+import { useSelector } from "react-redux";
+import { seeload } from "./../redux/loading";
+import { useDispatch } from "react-redux";
 
 export default function Home() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     document.title = "Online Shopping for Men & Women: The Da";
+    window.onload = () => {
+      dispatch(seeload({ start: "false" }));
+    };
   });
+
+  const load = useSelector((state) => state.load.start);
+
+  console.log(load);
   return (
     <div className="homeBodymDiv">
-      <Nav />
-      <Header />
-      <NewArrival />
-      <TopSelling />
-      <br />
-      CATEGORIES
-      <br />
-      Customer reviews
-      <br />
-      OFFICIAL MERCHANDISE/Brands
-      <br />
-      <br />
-      Hello world
-      <Footer />
+      {load == "true" ? (
+        <Loading />
+      ) : (
+        <div>
+          <Nav />
+          <Header />
+          <NewArrival />
+          <TopSelling />
+          <br />
+          CATEGORIES
+          <br />
+          Customer reviews
+          <br />
+          OFFICIAL MERCHANDISE/Brands
+          <br />
+          <br />
+          Hello world
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
