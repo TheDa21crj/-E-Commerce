69180828806e17a6c93e585fa36edf75efd639ef
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-23 21:33:40 +0530

    Update Header.js

--- font/src/Components/Home/Header.js
+++ font/src/Components/Home/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import Sliders from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import SlideText from "./SlideText";
 import HeaderCss from "./Css/Header.module.css";
 import "./Css/Header.css";
 
