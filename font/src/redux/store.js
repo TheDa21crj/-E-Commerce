f5bd2504e42e1590e235b1bf8136460b3d4bd5c6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-23 20:54:41 +0530

    Update store.js

--- font/src/redux/store.js
+++ font/src/redux/store.js
@@ -2,7 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import wishReducer from "./userSlice";
 import toggleReducer from "./toggleNav";
 import loading from "./loading";
+import sellingReducer from "./selling";
 
 export default configureStore({
-    reducer: { wish: wishReducer, toggle: toggleReducer, load: loading },
+    reducer: {
+        wish: wishReducer,
+        toggle: toggleReducer,
+        load: loading,
+        selling: sellingReducer,
+    },
 });
