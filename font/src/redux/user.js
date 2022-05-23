7252525ab889de6a257613a31ea93be20e18cc8f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-24 01:17:35 +0530

    Update user.js

--- font/src/redux/user.js
+++ font/src/redux/user.js
@@ -6,12 +6,20 @@ const userDataSlice = createSlice({
         _id: "",
         email: "",
         imgSrc: "",
+        firstName: "",
+        LastName: "",
+        gender: "",
+        PhoneNumber: "",
     },
     reducers: {
         adduser: (state, action) => {
             state._id = action.payload._id;
             state.email = action.payload.email;
             state.imgSrc = action.payload.imgSrc;
+            state.firstName = action.payload.firstName;
+            state.LastName = action.payload.LastName;
+            state.gender = action.payload.gender;
+            state.PhoneNumber = action.payload.PhoneNumber;
         },
     },
 });
