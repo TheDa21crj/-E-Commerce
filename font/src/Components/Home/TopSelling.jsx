c7479a68ee7b88a0aae2accb7878c89885003d31
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-25 15:38:01 +0530

    Update TopSelling.jsx

--- font/src/Components/Home/TopSelling.jsx
+++ font/src/Components/Home/TopSelling.jsx
@@ -20,6 +20,29 @@ export default function TopSelling() {
     slidesToShow: 4,
     slidesToScroll: 4,
     autoplay: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          // infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          // dots: true,
+        },
+      },
+      // {
+      //   breakpoint: 300,
+      //   settings: "unslick", // destroys slick
+      // },
+    ],
   };
 
   return (
