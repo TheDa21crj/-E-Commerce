c287b7d783fd25e292b858e2440063dd5c3e615f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-25 07:38:58 +0530

    Create Nav.jsx

---
+++ font/src/Components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavDes from "./NavDes";
+import NavMobo from "./NavMobo";
+import NavCss from "./Css/Nav.module.css";
+
+export default function Nav() {
+    return ( <
+        div >
+        <
+        NavDes / >
+        <
+        div className = { NavCss.underNav } > < /div> <
+        NavMobo / >
+        <
+        div className = { NavCss.underNav } > < /div> <
+        /div>
+    );
+}
