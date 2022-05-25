de787ac640a02ccf1c472afce7b98e54429db972
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-25 15:42:57 +0530

    Update NewArrival.jsx

--- font/src/Components/Home/NewArrival.jsx
+++ font/src/Components/Home/NewArrival.jsx
@@ -19,13 +19,6 @@ export default function NewArrival() {
     autoplay: false,
     mobileFirst: true,
     responsive: [
-      // {
-      //   breakpoint: 1024,
-      //   settings: {
-      //     slidesToShow: 3,
-      //     slidesToScroll: 3,
-      //   },
-      // },
       {
         breakpoint: 600,
         settings: {
