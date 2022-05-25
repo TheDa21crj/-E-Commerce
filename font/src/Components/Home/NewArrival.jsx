5044e96c8d8a46ee408fe26d88695d74fb75623e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-25 15:39:43 +0530

    Update NewArrival.jsx

--- font/src/Components/Home/NewArrival.jsx
+++ font/src/Components/Home/NewArrival.jsx
@@ -17,6 +17,23 @@ export default function NewArrival() {
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: false,
+    mobileFirst: true,
+    responsive: [
+      // {
+      //   breakpoint: 1024,
+      //   settings: {
+      //     slidesToShow: 3,
+      //     slidesToScroll: 3,
+      //   },
+      // },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
   };
 
   const selling = useSelector((state) => state.selling.newArrival);
