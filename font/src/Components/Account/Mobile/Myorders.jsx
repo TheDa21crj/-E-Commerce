83ed6efef6990744749d2f4c5172c31d5617042d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 12:55:59 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Mobile/Myorders.jsx
+++ font/src/Components/Account/Mobile/Myorders.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
 
-export default function Myorders() {
+export default function Myorders(props) {
   return <div>Myorders</div>;
 }
