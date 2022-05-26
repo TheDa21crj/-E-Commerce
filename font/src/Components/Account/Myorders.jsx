5a4b0ab9b8dc1eaefafb693c141d12fd0624b19e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 11:00:35 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Myorders.jsx
+++ font/src/Components/Account/Myorders.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import ProfileCss from "./CSS/Profile.module.css";
 
 export default function Myorders(props) {
   return (
-    <div>
+    <div
+      className={ProfileCss.mdiv}
+      style={{ height: "100vh", padding: "20px" }}
+    >
       <h1>My Orders</h1>
       <p>{props.id}</p>
       <KeyboardBackspaceIcon onClick={() => props.state("")} />
