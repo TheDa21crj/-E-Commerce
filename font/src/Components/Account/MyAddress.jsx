eb1d8aa99154b86c4204b0b17be07aa682abd136
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 12:45:01 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -4,13 +4,9 @@ import ProfileCss from "./CSS/Profile.module.css";
 
 export default function MyAddress(props) {
   return (
-    <div
-      className={ProfileCss.mdiv}
-      style={{ height: "100vh", padding: "20px" }}
-    >
+    <div className={ProfileCss.mdiv}>
       <h1>My Address</h1>
       <p>{props.id}</p>
-      <KeyboardBackspaceIcon onClick={() => props.state()} />
     </div>
   );
 }
