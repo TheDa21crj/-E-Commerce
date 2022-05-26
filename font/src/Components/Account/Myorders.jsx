61473ddea83d77edf7d4ee08d9fcd6b85874fca2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 19:02:01 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Myorders.jsx
+++ font/src/Components/Account/Myorders.jsx
@@ -4,8 +4,7 @@ import ProfileCss from "./CSS/Profile.module.css";
 export default function Myorders(props) {
   return (
     <div className={ProfileCss.mdiv}>
-      <h1>My Orders</h1>
-      <p>{props.id}</p>
+      <h1 className={ProfileCss.h1}>My Orders</h1>
     </div>
   );
 }
