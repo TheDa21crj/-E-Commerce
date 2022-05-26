22e649ca505a3ef39a0bdad97914ce93aaa940cc
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 13:40:52 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/Mobile/MyAddress.jsx
+++ font/src/Components/Account/Mobile/MyAddress.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
 
 export default function MyAddress(props) {
   return (
-    <div>
-      MyAddress
+    <div className={Common.mdiv}>
       <div>
         <KeyboardBackspaceIcon onClick={() => props.state("")} />
       </div>
+      MyAddress
     </div>
   );
 }
