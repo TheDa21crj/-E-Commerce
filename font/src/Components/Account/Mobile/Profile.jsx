6d7a47a185ff3da7db6ac7af3e3600cf7adc4f74
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 09:58:03 +0530

    Update Profile.jsx

--- font/src/Components/Account/Mobile/Profile.jsx
+++ font/src/Components/Account/Mobile/Profile.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import MPro from "./../CSS/MoboProbile.module.css";
+import CloseIcon from "@mui/icons-material/Close";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      Profile
+      <br />
+      <br />
+      <br />
+      <CloseIcon />
+    </div>
+  );
 }
