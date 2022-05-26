a0f5bbd606b9419e1a3d5bd328c91c96dc03326b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 13:48:28 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Mobile/Myorders.jsx
+++ font/src/Components/Account/Mobile/Myorders.jsx
@@ -11,7 +11,44 @@ export default function Myorders(props) {
           onClick={() => props.state("")}
         />
       </div>
-      <p>My Orders</p>
+      <p className={Common.MyPTag}>My Orders</p>
+      <p>{props.id}</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+      cupiditate tempora odio blanditiis corrupti qui eum hic deserunt alias
+      quasi provident placeat magnam officiis aspernatur quaerat perferendis!
+      Culpa, architecto assumenda? Omnis dolore illo magnam nostrum impedit
+      saepe? Rerum voluptatibus, odio laborum est iure odit deserunt saepe,
+      similique assumenda vitae dolore dolorem reprehenderit numquam animi qui
+      eius maxime perferendis et asperiores natus modi in accusantium? Fugiat
+      laboriosam voluptatum iste dolor mollitia? Corrupti, repudiandae facere!
+      Accusamus soluta maxime unde quos eius at doloribus. Deleniti, fugiat?
+      Iusto, aliquid magni voluptatum voluptatibus corrupti omnis quae! Magnam,
+      dolorem ea? Culpa ab ipsam id tempore dolorem! Lorem ipsum dolor sit amet
+      consectetur, adipisicing elit. Impedit, cupiditate tempora odio blanditiis
+      corrupti qui eum hic deserunt alias quasi provident placeat magnam
+      officiis aspernatur quaerat perferendis! Culpa, architecto assumenda?
+      Omnis dolore illo magnam nostrum impedit saepe? Rerum voluptatibus, odio
+      laborum est iure odit deserunt saepe, similique assumenda vitae dolore
+      dolorem reprehenderit numquam animi qui eius maxime perferendis et
+      asperiores natus modi in accusantium? Fugiat laboriosam voluptatum iste
+      dolor mollitia? Corrupti, repudiandae facere! Accusamus soluta maxime unde
+      quos eius at doloribus. Deleniti, fugiat? Iusto, aliquid magni voluptatum
+      voluptatibus corrupti omnis quae! Magnam, dolorem ea? Culpa ab ipsam id
+      tempore dolorem! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Impedit, cupiditate tempora odio blanditiis corrupti qui eum hic deserunt
+      alias quasi provident placeat magnam officiis aspernatur quaerat
+      perferendis! Culpa, architecto assumenda? Omnis dolore illo magnam nostrum
+      impedit saepe? Rerum voluptatibus, odio laborum est iure odit deserunt
+      saepe, similique assumenda vitae dolore dolorem reprehenderit numquam
+      animi qui eius maxime perferendis et asperiores natus modi in accusantium?
+      Fugiat laboriosam voluptatum iste dolor mollitia? Corrupti, repudiandae
+      facere! Accusamus soluta maxime unde quos eius at doloribus. Deleniti,
+      fugiat? Iusto, aliquid magni voluptatum voluptatibus corrupti omnis quae!
+      Magnam, dolorem ea? Culpa ab ipsam id tempore dolorem!
     </div>
   );
 }
