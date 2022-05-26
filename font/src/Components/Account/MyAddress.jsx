f28a53d5ae3a4f627808fa88a737ad5999cdb51e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 16:20:16 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProfileCss from "./CSS/Profile.module.css";
 
 export default function MyAddress(props) {
+  useEffect(() => {
+    address();
+  }, []);
+
+  const address = async () => {
+    // const { name, address, pinCode, town, state, country, phoneNumber } =
+    //   showUser;
+    const res = await fetch("/api/Address/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "rishav",
+        address: "st-70,qt-2a",
+        pinCode: "713331",
+        town: "crj",
+        state: "wb",
+        country: "india",
+        phoneNumber: "9939197376",
+      }),
+    });
+
+    const r = await res.json();
+    console.log(r);
+  };
+
   return (
     <div className={ProfileCss.mdiv}>
       <h1>My Address</h1>
