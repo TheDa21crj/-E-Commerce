567d17a1b7b1b804f4aa5a3271e7c9e83137d94a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 13:51:43 +0530

    Update Profile.jsx

--- font/src/Components/Account/Mobile/Profile.jsx
+++ font/src/Components/Account/Mobile/Profile.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
+import ProfileCssMobo from "./Css/Profile.module.css";
+import ProfileCss from "./../CSS/Profile.module.css";
 
 export default function Profile(props) {
   return (
@@ -12,6 +14,18 @@ export default function Profile(props) {
         />
       </div>
       <p className={Common.MyPTag}>Profile</p>
+      <div className={ProfileCss.EmailDiv}>
+        <p>Email ID:</p>
+        <input
+          type="email"
+          className={ProfileCss.impEmail}
+          name=""
+          id=""
+          readonly="readonly"
+          disabled="disabled"
+          value={props.email}
+        />
+      </div>
     </div>
   );
 }
