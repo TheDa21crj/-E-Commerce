a3c042bc19e65c773db25c59dc6ac12238426256
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 19:25:18 +0530

    Update AddAddress.jsx

--- font/src/Components/Account/AddAddress.jsx
+++ font/src/Components/Account/AddAddress.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import AddCss from "./CSS/Add.module.css";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function AddAddress() {
-  return <div>AddAddress</div>;
+export default function AddAddress(props) {
+  return (
+    <div>
+      AddAddress
+      <br />
+      <CloseIcon onClick={() => props.state(false)} />
+    </div>
+  );
 }
