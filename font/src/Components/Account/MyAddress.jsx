3f6193d7e9f161c7fc607150beb9e50dbfbadf37
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 10:23:50 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import CloseIcon from "@mui/icons-material/Close";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export default function MyAddress(props) {
   return (
     <div>
       <h1>My Address</h1>
       <p>{props.id}</p>
-      <CloseIcon onClick={() => props.state()} />
+      <KeyboardBackspaceIcon onClick={() => props.state()} />
     </div>
   );
 }
