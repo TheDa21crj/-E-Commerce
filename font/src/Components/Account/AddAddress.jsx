f35aa0e423067f03f1c2bcc126c36e32a363a29d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 20:09:59 +0530

    Update AddAddress.jsx

--- font/src/Components/Account/AddAddress.jsx
+++ font/src/Components/Account/AddAddress.jsx
@@ -3,31 +3,48 @@ import AddCss from "./CSS/Add.module.css";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function AddAddress(props) {
-  useEffect(() => {
-    address();
-  }, []);
+  const [showUser, setUser] = useState({
+    name: "",
+    address: "",
+    pinCode: "",
+    town: "",
+    state: "",
+    country: "",
+    phoneNumber: "",
+  });
+  // useEffect(() => {
+  //   address();
+  // }, []);
 
-  const address = async () => {
-    // const { name, address, pinCode, town, state, country, phoneNumber } =
-    // showUser;
+  const PostData = async () => {
+    const { name, address, pinCode, town, state, country, phoneNumber } =
+      showUser;
+
     const res = await fetch("/api/Address/add", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name: "rishav",
-        address: "st-70,qt-2a",
-        pinCode: "703330",
-        town: "crj",
-        state: "wb",
-        country: "india",
-        phoneNumber: "9939197376",
+        name,
+        address,
+        pinCode,
+        town,
+        state,
+        country,
+        phoneNumber,
       }),
     });
 
     const r = await res.json();
     console.log(r);
+  };
+
+  const DataInp = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUser({ ...showUser, [name]: value });
   };
 
   return (
@@ -45,45 +62,54 @@ export default function AddAddress(props) {
             type="text"
             name="name"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="Name"
           />
           <input
             type="number"
             name="pinCode"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="Pin Code"
           />
           <input
             type="text"
             name="address"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="Address"
           />
           <input
             type="text"
             name="town"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="Town"
           />
           <input
             type="text"
             name="state"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="State"
           />
           <input
             type="text"
             name="country"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="Country"
           />
           <input
             type="text"
             name="phoneNumber"
             className={AddCss.impTag}
+            onChange={DataInp}
             placeholder="Phone Number"
           />
-          <button className={AddCss.LoginBtn}>ADD</button>
+          <button onClick={PostData} className={AddCss.LoginBtn}>
+            ADD
+          </button>
         </div>
       </div>
     </div>
