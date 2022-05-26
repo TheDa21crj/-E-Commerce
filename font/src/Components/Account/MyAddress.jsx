570f9cc70292944cacf848db12c71d2ab45bd921
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 11:00:33 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import ProfileCss from "./CSS/Profile.module.css";
 
 export default function MyAddress(props) {
   return (
-    <div>
+    <div
+      className={ProfileCss.mdiv}
+      style={{ height: "100vh", padding: "20px" }}
+    >
       <h1>My Address</h1>
       <p>{props.id}</p>
       <KeyboardBackspaceIcon onClick={() => props.state()} />
