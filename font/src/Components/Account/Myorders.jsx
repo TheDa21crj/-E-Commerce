35bfe1a313d15351aec6b2925b7a58f58e4854ea
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 10:23:52 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Myorders.jsx
+++ font/src/Components/Account/Myorders.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import CloseIcon from "@mui/icons-material/Close";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export default function Myorders(props) {
   return (
     <div>
       <h1>My Orders</h1>
       <p>{props.id}</p>
-      <CloseIcon onClick={() => props.state("")} />
+      <KeyboardBackspaceIcon onClick={() => props.state("")} />
     </div>
   );
 }
