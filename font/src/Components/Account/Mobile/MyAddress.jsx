62306869f6a369db48dbad682ce98717ce47b649
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 12:55:56 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/Mobile/MyAddress.jsx
+++ font/src/Components/Account/Mobile/MyAddress.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
 
-export default function MyAddress() {
+export default function MyAddress(props) {
   return <div>MyAddress</div>;
 }
