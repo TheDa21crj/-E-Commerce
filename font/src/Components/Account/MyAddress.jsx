8a48a6c514a15378550cfc2f8afd5faf9d82babe
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 09:57:56 +0530

    Create MyAddress.jsx

---
+++ font/src/Components/Account/MyAddress.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function MyAddress(props) {
+  return (
+    <div>
+      <h1>My Address</h1>
+      <p>{props.id}</p>
+      <CloseIcon />
+    </div>
+  );
+}
