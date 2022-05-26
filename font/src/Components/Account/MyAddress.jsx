a6143716e55a97880a0da83e1d704ca5adfd6b92
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 17:30:13 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -5,32 +5,59 @@ import AddIcon from "@mui/icons-material/Add";
 import imgplus from "./../../Img/plus.png";
 
 export default function MyAddress(props) {
-  // useEffect(() => {
-  //   address();
-  // }, []);
+  useEffect(() => {
+    address();
+    seeAddress();
+  }, []);
 
-  // const address = async () => {
-  // const { name, address, pinCode, town, state, country, phoneNumber } =
-  //   showUser;
-  // const res = await fetch("/api/Address/add", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     name: "rishav",
-  //     address: "st-70,qt-2a",
-  //     pinCode: "815354",
-  //     town: "crj",
-  //     state: "wb",
-  //     country: "india",
-  //     phoneNumber: "9939197376",
-  //   }),
-  // });
+  const seeAddress = async () => {
+    try {
+      const res = await fetch("/api/Address", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (data.errors) {
+        return;
+      }
+      if (data) {
+        if (data.message == "zero") {
+          return;
+        } else {
+          console.table(data.message);
+        }
+      }
+    } catch (error) {
+      return;
+    }
+  };
 
-  //   const r = await res.json();
-  //   console.log(r);
-  // };
+  const address = async () => {
+    // const { name, address, pinCode, town, state, country, phoneNumber } =
+    // showUser;
+    const res = await fetch("/api/Address/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "rishav",
+        address: "st-70,qt-2a",
+        pinCode: "815354",
+        town: "crj",
+        state: "wb",
+        country: "india",
+        phoneNumber: "9939197376",
+      }),
+    });
+
+    const r = await res.json();
+    console.log(r);
+  };
 
   return (
     <div className={ProfileCss.mdiv}>
