9522e9b894d0093102e324cc4a555dbad8aef39a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 12:55:17 +0530

    Update Profile.jsx

--- font/src/Components/Account/Mobile/Profile.jsx
+++ font/src/Components/Account/Mobile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import Common from "./Css/Common.module.css";
 
 export default function Profile() {
   return <div>Profile</div>;
