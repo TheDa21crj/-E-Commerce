68416395498d215fea1130dd08f257c6312ebc48
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 13:40:58 +0530

    Update Profile.jsx

--- font/src/Components/Account/Mobile/Profile.jsx
+++ font/src/Components/Account/Mobile/Profile.jsx
@@ -5,10 +5,10 @@ import Common from "./Css/Common.module.css";
 export default function Profile(props) {
   return (
     <div className={Common.mdiv}>
-      Profile
       <div>
         <KeyboardBackspaceIcon onClick={() => props.state("")} />
       </div>
+      Profile
     </div>
   );
 }
