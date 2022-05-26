618499c0dbf9f8e50015f05caa6836e785a9c3d9
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-26 17:20:04 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -1,38 +1,48 @@
 import React, { useEffect } from "react";
 import ProfileCss from "./CSS/Profile.module.css";
+import AddCss from "./CSS/Address.module.css";
+import AddIcon from "@mui/icons-material/Add";
+import imgplus from "./../../Img/plus.png";
 
 export default function MyAddress(props) {
-  useEffect(() => {
-    address();
-  }, []);
+  // useEffect(() => {
+  //   address();
+  // }, []);
 
-  const address = async () => {
-    // const { name, address, pinCode, town, state, country, phoneNumber } =
-    //   showUser;
-    const res = await fetch("/api/Address/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: "rishav",
-        address: "st-70,qt-2a",
-        pinCode: "815354",
-        town: "crj",
-        state: "wb",
-        country: "india",
-        phoneNumber: "9939197376",
-      }),
-    });
+  // const address = async () => {
+  // const { name, address, pinCode, town, state, country, phoneNumber } =
+  //   showUser;
+  // const res = await fetch("/api/Address/add", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     name: "rishav",
+  //     address: "st-70,qt-2a",
+  //     pinCode: "815354",
+  //     town: "crj",
+  //     state: "wb",
+  //     country: "india",
+  //     phoneNumber: "9939197376",
+  //   }),
+  // });
 
-    const r = await res.json();
-    console.log(r);
-  };
+  //   const r = await res.json();
+  //   console.log(r);
+  // };
 
   return (
     <div className={ProfileCss.mdiv}>
-      <h1>My Address</h1>
-      <p>{props.id}</p>
+      <h1 className={ProfileCss.h1}>My Address</h1>
+
+      <div className={AddCss.AddressmDiv}>
+        <div className={AddCss.AddAddressmDiv}>
+          <img src={imgplus} alt="" />
+          <p>ADD ADDRESS</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
