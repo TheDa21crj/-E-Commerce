32d4353ded0e1f4817ed10ebf4c5659bc5da1fbd
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-14 19:56:18 +0530

    Update DeleteAddress.jsx

--- font/src/Components/Account/DeleteAddress.jsx
+++ font/src/Components/Account/DeleteAddress.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DeleteCss from "./CSS/Delete.module.css";
 
 export default function DeleteAddress(props) {
+  useEffect(() => {
+    // setInterval(function () {
+    console.log(props.DeleteID);
+    // }, 5000);
+  }, [props.DeleteID]);
+
   return (
     <div className={DeleteCss.mdiv}>
       Delete Address
