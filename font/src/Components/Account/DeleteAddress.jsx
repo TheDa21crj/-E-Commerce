eaf7998cc82c57bfe7782453fa39c82a744ef48f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-16 14:53:01 +0530

    Update DeleteAddress.jsx

--- font/src/Components/Account/DeleteAddress.jsx
+++ font/src/Components/Account/DeleteAddress.jsx
@@ -30,5 +30,10 @@ export default function DeleteAddress(props) {
     }
   };
 
-  return <div className={DeleteCss.mdiv}>Delete Address</div>;
+  return (
+    <div className={DeleteCss.mdiv}>
+      Delete Address
+      {/* jj */}
+    </div>
+  );
 }
