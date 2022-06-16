b15369c225fbf22b8c7e17c0ead55e9d3aa08222
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-16 20:20:46 +0530

    Update ShopCart.jsx

--- font/src/Pages/ShopCart.jsx
+++ font/src/Pages/ShopCart.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../Components/Nav";
+import SCCss from "./Css/ShopCart.module.css";
 
 export default function ShopCart() {
   useEffect(() => {
@@ -8,7 +9,7 @@ export default function ShopCart() {
   return (
     <div>
       <Nav />
-      Shop Cart
+      <p className={SCCss.MainPTag}>Shop Cart</p>
     </div>
   );
 }
