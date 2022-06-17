4ae92a37cbaec466f82446064d9299fdf5aff56f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-17 14:03:14 +0530

    Update Shopping.js

--- routes/Shopping.js
+++ routes/Shopping.js
@@ -71,6 +71,19 @@ router.post(
 );
 
 // Private || See Shoping || api/Shoping
-router.get("/api/Shoping", UserAuth, async(req, res) => {});
+router.get("/", UserAuth, async(req, res) => {
+    let userID = req.userId;
+    try {
+        let userCheck = await Shoping.findOne({ user: userID });
+        if (userCheck) {
+            return res.status(200).json({ message: userCheck.List });
+        } else {
+            return res.status(200).json({ message: "zero" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error });
+    }
+});
 
 module.exports = router;
