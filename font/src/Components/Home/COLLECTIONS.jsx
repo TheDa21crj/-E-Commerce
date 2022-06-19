de4f479f64285884e55c5d02d6555839b5ccef08
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 18:28:38 +0530

    Update COLLECTIONS.jsx

--- font/src/Components/Home/COLLECTIONS.jsx
+++ font/src/Components/Home/COLLECTIONS.jsx
@@ -3,6 +3,7 @@ import TSCss from "./Css/TopSelling.module.css";
 import Sliders from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CollectionJson from "./JSON/Collection.json";
 import CollCss from "./Css/COLLECTIONS.module.css";
 
 export default function COLLECTIONS() {
@@ -21,7 +22,15 @@ export default function COLLECTIONS() {
       <div className={TSCss.H1Div}>
         <h1 className={TSCss.h1}>COLLECTIONS</h1>
       </div>
-      <Sliders {...settingTS} className={TSCss.Hslide}></Sliders>
+      <Sliders {...settingTS} className={TSCss.Hslide}>
+        {CollectionJson.map((value, key) => {
+          return (
+            <div className={CollCss.MapMDiv} key={key}>
+              hello
+            </div>
+          );
+        })}
+      </Sliders>
     </div>
   );
 }
