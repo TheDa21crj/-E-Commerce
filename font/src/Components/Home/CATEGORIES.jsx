84113ddd9204892052806048ff61b10a6ab4e903
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 16:07:17 +0530

    Update CATEGORIES.jsx

--- font/src/Components/Home/CATEGORIES.jsx
+++ font/src/Components/Home/CATEGORIES.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import TSCss from "./Css/TopSelling.module.css";
 
 export default function CATEGORIES() {
-  return <div>CATEGORIES C</div>;
+  return (
+    <div>
+      <div className={TSCss.H1Div}>
+        <h1 className={TSCss.h1}>CATEGORIES</h1>
+      </div>
+    </div>
+  );
 }
