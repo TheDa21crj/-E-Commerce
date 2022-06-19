5ec1342546f8792e936bdb6fc22a693a410f59ae
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 17:41:12 +0530

    Update COLLECTIONS.jsx

--- font/src/Components/Home/COLLECTIONS.jsx
+++ font/src/Components/Home/COLLECTIONS.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function COLLECTIONS() {
-  return <div>COLLECTIONS</div>;
+  return (
+    <div className="MERCHANDISEmDiv">
+      <div className={TSCss.H1Div}>
+        <h1 className={TSCss.h1}>COLLECTIONS</h1>
+      </div>
+    </div>
+  );
 }
