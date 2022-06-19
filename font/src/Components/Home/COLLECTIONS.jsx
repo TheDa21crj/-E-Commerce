c370e70a8630697ada82759031d85ddaa09ec733
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 18:21:48 +0530

    Update COLLECTIONS.jsx

--- font/src/Components/Home/COLLECTIONS.jsx
+++ font/src/Components/Home/COLLECTIONS.jsx
@@ -3,6 +3,7 @@ import TSCss from "./Css/TopSelling.module.css";
 import Sliders from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CollCss from "COLLECTIONS.module.css";
 
 export default function COLLECTIONS() {
   var settingTS = {
