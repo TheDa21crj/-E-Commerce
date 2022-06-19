f460ccf95a91fd5d5689e6eb9527f4fd913f737e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 17:34:45 +0530

    Update Home.jsx

--- font/src/Pages/Home.jsx
+++ font/src/Pages/Home.jsx
@@ -80,7 +80,6 @@ export default function Home() {
           <CATEGORIES />
           <TopSelling />
           <COLLECTIONS />
-          <br />
           <MERCHANDISE />
           <Footer />
         </div>
