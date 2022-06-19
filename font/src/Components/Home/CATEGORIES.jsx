6b39b8988aa8cb1b548bf7e84a604ae5bc192720
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 16:17:39 +0530

    Update CATEGORIES.jsx

--- font/src/Components/Home/CATEGORIES.jsx
+++ font/src/Components/Home/CATEGORIES.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import TSCss from "./Css/TopSelling.module.css";
+import data from "./data.json";
 
 export default function CATEGORIES() {
+  console.table(data);
   return (
     <div>
       <div className={TSCss.H1Div}>
         <h1 className={TSCss.h1}>CATEGORIES</h1>
+      </div>
+      <div className="hello">
+        {data.map((value, key) => {
+          return <div key={value.id}>{value.name}</div>;
+        })}
       </div>
     </div>
   );
