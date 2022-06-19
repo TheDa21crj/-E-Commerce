4691994d5dac613de86fd5a4b9824f04d6ab1af6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-19 17:54:45 +0530

    Update COLLECTIONS.jsx

--- font/src/Components/Home/COLLECTIONS.jsx
+++ font/src/Components/Home/COLLECTIONS.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import TSCss from "./Css/TopSelling.module.css";
+import Sliders from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function COLLECTIONS() {
+  var settingTS = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    mobileFirst: true,
+  };
   return (
     <div className="MERCHANDISEmDiv">
       <div className={TSCss.H1Div}>
         <h1 className={TSCss.h1}>COLLECTIONS</h1>
       </div>
+      <Sliders {...settingTS} className={TSCss.Hslide}></Sliders>
     </div>
   );
 }
