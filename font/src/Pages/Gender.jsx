f2e3dba50ff1c93205d03cb9a45479e98a3cdbd3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-26 20:19:13 +0530

    Update Gender.jsx

--- font/src/Pages/Gender.jsx
+++ font/src/Pages/Gender.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function Gender() {
+  const { gender } = useParams();
+
   return <div>Gender</div>;
 }
