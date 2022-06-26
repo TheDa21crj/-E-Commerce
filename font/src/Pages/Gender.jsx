6f7c1a7ce21c61c99dadca8fc2255509d71bbafb
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-26 20:23:55 +0530

    Update Gender.jsx

--- font/src/Pages/Gender.jsx
+++ font/src/Pages/Gender.jsx
@@ -4,5 +4,30 @@ import { Link, useParams } from "react-router-dom";
 export default function Gender() {
   const { gender } = useParams();
 
+  const DataGet = async () => {
+    try {
+      const res = await fetch("/api/admin/Products/Gender", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          gender,
+        }),
+        credentials: "include",
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    DataGet();
+  });
+
   return <div>Gender</div>;
 }
