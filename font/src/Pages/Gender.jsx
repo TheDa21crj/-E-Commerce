37f47ab238a04ec6bb0b0a3106de0227df596eb9
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-27 08:45:12 +0530

    Update Gender.jsx

--- font/src/Pages/Gender.jsx
+++ font/src/Pages/Gender.jsx
@@ -41,12 +41,8 @@ export default function Gender() {
       <div className={GenCss.mDiv}>
         <div className={GenCss.SelectDiv}>
           <select name="sort" id="" className={GenCss.selectTag}>
-            <option value="low" className={GenCss.optionTag}>
-              Low to High
-            </option>
-            <option value="high" className={GenCss.optionTag}>
-              High to Low
-            </option>
+            <option value="low">Low to High</option>
+            <option value="high">High to Low</option>
           </select>
         </div>
         {showGender ? (
