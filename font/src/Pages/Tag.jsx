420b9a70d4e43ac7c8f84418ed1bf9dbc4d2885b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-27 15:07:59 +0530

    Update Tag.jsx

--- font/src/Pages/Tag.jsx
+++ font/src/Pages/Tag.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../Components/Nav";
 import { Link, useParams } from "react-router-dom";
+import GenCss from "./Css/Gender.module.css";
+import TSCss from "./../Components/Home/Css/TopSelling.module.css";
+import StarIcon from "@mui/icons-material/Star";
+import Footer from "../Components/Footer";
 
 export default function Tag() {
   const [showGender, setGender] = useState();
@@ -36,6 +40,36 @@ export default function Tag() {
   return (
     <div>
       <Nav />
+      {showGender ? (
+        <div className={GenCss.MapPDiv}>
+          {showGender.map((value, key) => {
+            return (
+              <div key={value._id} className={GenCss.MapMDiv}>
+                <Link to={`/products/${value._id}`} className="LinkStyle">
+                  <div>
+                    <img
+                      src={value.imageSrc}
+                      alt={value.name}
+                      className={GenCss.ImgSrc}
+                    />
+                  </div>
+                  <div className={TSCss.DetailsDiv}>
+                    <p className={TSCss.Name}> {value.name} </p>
+                    <p className={TSCss.Rating}>
+                      <StarIcon fontSize="small" className={TSCss.StarIcon} />
+                      <span> {value.rating} </span>
+                    </p>
+                    <p className={TSCss.price}> ₹{value.price} </p>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        ""
+      )}
+      <Footer />
     </div>
   );
 }
