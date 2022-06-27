df32e47cefe41b189fa1d032538bc429396fcb13
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-27 15:05:36 +0530

    Update Tag.jsx

--- font/src/Pages/Tag.jsx
+++ font/src/Pages/Tag.jsx
@@ -5,6 +5,31 @@ import { Link, useParams } from "react-router-dom";
 export default function Tag() {
   const { tag, gender } = useParams();
 
+  const DataGet = async () => {
+    try {
+      const res = await fetch("/api/admin/Products/Tag", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          gender,
+          tag,
+        }),
+      });
+
+      const data = await res.json();
+      setGender(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    DataGet();
+  }, [gender]);
+
   return (
     <div>
       <Nav />
