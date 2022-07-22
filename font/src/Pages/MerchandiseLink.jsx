c8954c9321bdb2274ac1f9688ab6c36ef66fa592
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-23 02:55:22 +0530

    Update MerchandiseLink.jsx

--- font/src/Pages/MerchandiseLink.jsx
+++ font/src/Pages/MerchandiseLink.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function MerchandiseLink() {
   const { link } = useParams();
 
+  const AuthMiddleware = async () => {
+    try {
+      const res = await fetch("/api/admin/Products/MERCHANDISE", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          tag: link,
+        }),
+      });
+
+      const data = await res.json();
+      if (data.errors) {
+      } else {
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    AuthMiddleware();
+  }, []);
   return (
     <div>
       MerchandiseLink
