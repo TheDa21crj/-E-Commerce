d590692ba2b02e45a2e8dbc49da5022b92c0941e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-23 03:33:05 +0530

    Update MERCHANDISE.jsx

--- font/src/Components/Home/MERCHANDISE.jsx
+++ font/src/Components/Home/MERCHANDISE.jsx
@@ -4,16 +4,6 @@ import TSCss from "./Css/TopSelling.module.css";
 import { Link } from "react-router-dom";
 
 // img
-import img1 from "./../../Img/MERCHANDISE/DC.webp";
-import img2 from "./../../Img/MERCHANDISE/Disney.webp";
-import img3 from "./../../Img/MERCHANDISE/friends.webp";
-import img4 from "./../../Img/MERCHANDISE/HarryPotter.webp";
-import img5 from "./../../Img/MERCHANDISE/himym.jpeg";
-import img6 from "./../../Img/MERCHANDISE/LooneyTunes.webp";
-import img7 from "./../../Img/MERCHANDISE/lucifer.jpg";
-import img8 from "./../../Img/MERCHANDISE/Marvel_Logo.webp";
-import img9 from "./../../Img/MERCHANDISE/StarWars.webp";
-import img10 from "./../../Img/MERCHANDISE/web-icon.webp";
 
 export default function MERCHANDISE() {
   return (
