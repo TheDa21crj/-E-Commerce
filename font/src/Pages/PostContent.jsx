d7d3b33bf2267e8691bce9a3b44aa595b59e0871
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-22 23:00:32 +0530

    Update PostContent.jsx

--- font/src/Pages/PostContent.jsx
+++ font/src/Pages/PostContent.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import PostContentCom from "../Components/Admin/PostContent";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import PCcss from "./Css/PostContent.module.css";
+import PCcs from "./Css/PostContent.module.css";
 
 export default function PostContent() {
+  const [change, setchange] = useState("1");
   return (
     <div>
-      <div className={PCcss.headdiv}>
-        <Link to="/" className={PCcss.titleNav}>
+      <div className={PCcs.headdiv}>
+        <Link to="/" className={PCcs.titleNav}>
           <h1>The Da</h1>
         </Link>
         Logout
       </div>
-      <h1 className={PCcss.h1WelAdmin}>Welcome Admin</h1>
+      <h1 className={PCcs.h1WelAdmin}>Welcome Admin</h1>
+      <div className={PCcs.postPDiv}>
+        <p className={PCcs.postPTag}>Post New Product</p>
+        <p className={PCcs.postPTag}>Update a Product</p>
+        <p></p>
+      </div>
       <PostContentCom />
     </div>
   );
