bbd376066d45c8f2186c4eac8575febd2c13908a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-23 02:08:08 +0530

    Update UpdateProduct.jsx

--- font/src/Components/Admin/UpdateProduct.jsx
+++ font/src/Components/Admin/UpdateProduct.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
 import PCcss from "./Css/PostContent.module.css";
+import UPCss from "./Css/Update.module.css";
 
 export default function UpdateProduct() {
+  const [showProduct, setProduct] = useState({
+    _id: "",
+    feild: "",
+    value: "",
+  });
   return (
-    <div>
+    <div className={UPCss.mDiv}>
       <input
         type="text"
         name="_id"
         id=""
-        // className={PCcss.ImpTag}
+        className={PCcss.ImpTag}
         placeholder="_id"
         // value={showProduct.name}
         // onChange={DataInpProduct}
@@ -17,7 +23,7 @@ export default function UpdateProduct() {
         type="text"
         name="field"
         id=""
-        // className={PCcss.ImpTag}
+        className={PCcss.ImpTag}
         placeholder="field"
         // value={showProduct.name}
         // onChange={DataInpProduct}
@@ -26,7 +32,7 @@ export default function UpdateProduct() {
         type="text"
         name="value"
         id=""
-        // className={PCcss.ImpTag}
+        className={PCcss.ImpTag}
         placeholder="value"
         // value={showProduct.name}
         // onChange={DataInpProduct}
