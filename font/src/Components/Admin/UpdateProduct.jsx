66b94241ba479ca8558f1e28063cc13e42163dac
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-23 02:03:15 +0530

    Update UpdateProduct.jsx

--- font/src/Components/Admin/UpdateProduct.jsx
+++ font/src/Components/Admin/UpdateProduct.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PCcss from "./Css/PostContent.module.css";
 
 export default function UpdateProduct() {
-  return <div>Update Product</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        name="_id"
+        id=""
+        // className={PCcss.ImpTag}
+        placeholder="_id"
+        // value={showProduct.name}
+        // onChange={DataInpProduct}
+      />
+      <input
+        type="text"
+        name="field"
+        id=""
+        // className={PCcss.ImpTag}
+        placeholder="field"
+        // value={showProduct.name}
+        // onChange={DataInpProduct}
+      />
+      <input
+        type="text"
+        name="value"
+        id=""
+        // className={PCcss.ImpTag}
+        placeholder="value"
+        // value={showProduct.name}
+        // onChange={DataInpProduct}
+      />
+    </div>
+  );
 }
