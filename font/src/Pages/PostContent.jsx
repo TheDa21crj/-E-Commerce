a2688f00e32e91d9fbf4151d0e5260458cae8522
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-22 22:44:05 +0530

    Update PostContent.jsx

--- font/src/Pages/PostContent.jsx
+++ font/src/Pages/PostContent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PostContentCom from "../Components/Admin/PostContent";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import PCcss from "./Css/PostContent.module.css";
 
 export default function PostContent() {
   return (
@@ -12,6 +13,8 @@ export default function PostContent() {
         </Link>
         Logout
       </div>
+      <h1 className={PCcss.h1WelAdmin}>Welcome Admin</h1>
+      <PostContentCom />
     </div>
   );
 }
