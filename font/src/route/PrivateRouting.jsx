3e3de6f2df8e37c6cfe475a84f89cfe070f3f78b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-13 20:11:37 +0530

    Update PrivateRouting.jsx

--- font/src/route/PrivateRouting.jsx
+++ font/src/route/PrivateRouting.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { adduser } from "./../redux/user";
@@ -8,14 +9,27 @@ import { adduser } from "./../redux/user";
 //   return email !== "" ? <Outlet /> : <Navigate to="/login" />;
 // };
 
-function PrivateRoute({ children, ...rest }) {
-  const email = useSelector((state) => state.user.email);
-  return (
-    <Route
-      {...rest}
-      render={() => (email !== "" ? children : <Redirect to="/login" />)}
-    />
-  );
-}
+// import React from "react";
+// import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const auth = null; // determine if authorized, from context or however you're doing it
+
+  // If authorized, return an outlet that will render child elements
+  // If not, return element that will navigate to login page
+  return auth ? <Outlet /> : <Navigate to="/login" />;
+};
+
+// function PrivateRoute({ children, ...rest }) {
+//   const email = useSelector((state) => state.user.email);
+//   return (
+//     <Route
+//       {...rest}
+//       render={() =>
+//         email !== "" ? console.log(children) : <Navigate to="/login" />
+//       }
+//     />
+//   );
+// }
 
 export default PrivateRoute;
