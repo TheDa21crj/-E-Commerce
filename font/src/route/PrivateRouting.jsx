7a2d374e4d9c11a4f1a6162b5ec73ed0cf57a52f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-13 20:06:43 +0530

    Update PrivateRouting.jsx

--- font/src/route/PrivateRouting.jsx
+++ font/src/route/PrivateRouting.jsx
@@ -3,22 +3,19 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { adduser } from "./../redux/user";
 
-const PrivateRoute = () => {
+// const PrivateRoute = () => {
+//   const email = useSelector((state) => state.user.email);
+//   return email !== "" ? <Outlet /> : <Navigate to="/login" />;
+// };
+
+function PrivateRoute({ children, ...rest }) {
   const email = useSelector((state) => state.user.email);
-  return email !== "" ? <Outlet /> : <Navigate to="/login" />;
-};
+  return (
+    <Route
+      {...rest}
+      render={() => (email !== "" ? children : <Redirect to="/login" />)}
+    />
+  );
+}
 
 export default PrivateRoute;
-
-// function PrivateRoute({ children, ...rest }) {
-//   let auth = useAuth();
-//   return (
-//     <Route
-//       {...rest}
-//       render={() => auth
-//         ? children
-//         : <Redirect to="/login" />
-//       }
-//     />
-//   );
-// }
