4f39a5c1cd2ec92761b83dc2bf0b51f33dbf5208
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-14 16:25:49 +0530

    Update PrivateRouting.jsx

--- font/src/route/PrivateRouting.jsx
+++ font/src/route/PrivateRouting.jsx
@@ -6,7 +6,6 @@ import { adduser } from "./../redux/user";
 
 const PrivateRoute = () => {
   const email = useSelector((state) => state.user.email);
-
   return email ? <p>Hello</p> : <p>email = {email}</p>;
 };
 
