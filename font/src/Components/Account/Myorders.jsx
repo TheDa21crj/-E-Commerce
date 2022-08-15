1c4a42e7989f5d92f5dfe6da18dc5fecb80a8ea2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-15 16:37:05 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Myorders.jsx
+++ font/src/Components/Account/Myorders.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ProfileCss from "./CSS/Profile.module.css";
+import OrderCss from "./CSS/Orders.module.css";
 
 export default function Myorders(props) {
   return (
     <div className={ProfileCss.mdiv}>
       <h1 className={ProfileCss.h1}>My Orders</h1>
-      <div className={ProfileCss.EmailDiv}>
-        <p>Email ID:</p>
-      </div>
+      {/* <div className={OrderCss.pTagMain}> */}
+      <p className={OrderCss.pTagMain}>No Orders</p>
+      {/* </div> */}
     </div>
   );
 }
