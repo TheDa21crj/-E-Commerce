faa88bbb2d9dbb24e9dd68244ee615c52a17f547
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-15 16:35:02 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Myorders.jsx
+++ font/src/Components/Account/Myorders.jsx
@@ -5,6 +5,9 @@ export default function Myorders(props) {
   return (
     <div className={ProfileCss.mdiv}>
       <h1 className={ProfileCss.h1}>My Orders</h1>
+      <div className={ProfileCss.EmailDiv}>
+        <p>Email ID:</p>
+      </div>
     </div>
   );
 }
