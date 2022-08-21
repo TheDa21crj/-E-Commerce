672d085829aa4998be330cc6f2177a58b319ff23
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-21 09:22:44 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/Mobile/MyAddress.jsx
+++ font/src/Components/Account/Mobile/MyAddress.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { adduser } from "./../../../redux/user";
 
 export default function MyAddress(props) {
   return (
