0df999646e4b4e77579812b6e48ee28556dcf23d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-21 10:10:13 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/Mobile/MyAddress.jsx
+++ font/src/Components/Account/Mobile/MyAddress.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
+import { useSelector } from "react-redux";
 
 export default function MyAddress(props) {
+  const toggle = useSelector((state) => state.toggle.toggle);
+
   return (
     <div className={Common.mdiv}>
       <div>
