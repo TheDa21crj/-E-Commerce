51f9ab5389190f0f55a3a2234cce3bd0bbffec05
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-21 09:09:53 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Mobile/Myorders.jsx
+++ font/src/Components/Account/Mobile/Myorders.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Common from "./Css/Common.module.css";
+import OrderCss from "./CSS/Orders.module.css";
 
 export default function Myorders(props) {
   return (
@@ -12,6 +13,8 @@ export default function Myorders(props) {
         />
       </div>
       <p className={Common.MyPTag}>My Orders</p>
+      <p className={OrderCss.pTagMain}>No Orders</p>
+      <br />
       <p>{props.id}</p>
     </div>
   );
