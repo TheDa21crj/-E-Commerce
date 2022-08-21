4dba7a786acf9d88adca5baea15f18fad4adb684
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-08-21 09:14:00 +0530

    Update user.js

--- font/src/redux/user.js
+++ font/src/redux/user.js
@@ -1,27 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userDataSlice = createSlice({
-    name: "userData",
-    initialState: {
-        _id: "",
-        email: "",
-        imgSrc: "",
-        firstName: "",
-        LastName: "",
-        gender: "",
-        PhoneNumber: "",
+  name: "userData",
+  initialState: {
+    _id: "",
+    email: "",
+    imgSrc: "",
+    firstName: "",
+    LastName: "",
+    gender: "",
+    PhoneNumber: "",
+    address: [],
+  },
+  reducers: {
+    adduser: (state, action) => {
+      state._id = action.payload._id;
+      state.email = action.payload.email;
+      state.imgSrc = action.payload.imgSrc;
+      state.firstName = action.payload.firstName;
+      state.LastName = action.payload.LastName;
+      state.gender = action.payload.gender;
+      state.PhoneNumber = action.payload.PhoneNumber;
     },
-    reducers: {
-        adduser: (state, action) => {
-            state._id = action.payload._id;
-            state.email = action.payload.email;
-            state.imgSrc = action.payload.imgSrc;
-            state.firstName = action.payload.firstName;
-            state.LastName = action.payload.LastName;
-            state.gender = action.payload.gender;
-            state.PhoneNumber = action.payload.PhoneNumber;
-        },
-    },
+  },
 });
 
 export const { adduser } = userDataSlice.actions;
