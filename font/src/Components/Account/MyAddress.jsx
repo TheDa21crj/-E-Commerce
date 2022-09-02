dd1384229efab25706e8623fb31acfada9351f8c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-03 02:18:20 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/MyAddress.jsx
+++ font/src/Components/Account/MyAddress.jsx
@@ -11,6 +11,7 @@ import { useDispatch } from "react-redux";
 import { adduser } from "./../../redux/user";
 
 export default function MyAddress(props) {
+  const [showload, setload] = useState(true);
   const [show, set] = useState();
   const [showAdd, setAdd] = useState(false);
   const [showDelete, setDelete] = useState(false);
@@ -38,8 +39,10 @@ export default function MyAddress(props) {
       }
       if (data) {
         if (data.message === "zero") {
+          setload(false);
           return;
         } else {
+          setload(false);
           set(data.message);
           dispatch({ address: data.message });
         }
@@ -63,70 +66,83 @@ export default function MyAddress(props) {
   }, [showAdd]);
 
   return (
-    <div className={ProfileCss.mdiv} id={AddCss.mDiv}>
-      <h1 className={ProfileCss.h1}>My Address</h1>
+    <>
+      {showload ? (
+        <Loading />
+      ) : (
+        <div className={ProfileCss.mdiv} id={AddCss.mDiv}>
+          <h1 className={ProfileCss.h1}>My Address</h1>
 
-      <div className={AddCss.AddressRowDiv}>
-        <div className={AddCss.mapPArent}>
-          {show ? (
-            <div className={AddCss.mapPArentDiv}>
-              <div
-                className={AddCss.AddAddressmDiv}
-                onClick={() => setAdd(true)}
-              >
-                <img src={imgplus} alt="" />
-                <p>ADD ADDRESS</p>
-              </div>
-              {show.map((value, key) => {
-                return (
-                  <div key={value._id} className={AddCss.AmDiv}>
-                    <p>
-                      <b>name:</b> {value.name}
-                    </p>
-                    <p>
-                      <b>address:</b> {value.address}
-                    </p>
-                    <p>
-                      <b>pin Code:</b> {value.pinCode}
-                    </p>
-                    <p>
-                      <b>Town:</b> {value.town}
-                    </p>
-                    <p>
-                      <b>State:</b> {value.state}
-                    </p>
-                    <p>
-                      <b>Country:</b> {value.country}
-                    </p>
-                    <p>
-                      <b>Phone Number:</b> {value.phoneNumber}
-                    </p>
-                    <div className={AddCss.editHoverDiv}>
-                      <p className={AddCss.editHoverEdit}>{/* Edit */}</p>
-                      <p
-                        className={AddCss.editHoverDelete}
-                        onClick={() => {
-                          setDelete(true);
-                          setDeleteID(value._id);
-                        }}
-                      >
-                        Delete
-                      </p>
-                    </div>
+          <div className={AddCss.AddressRowDiv}>
+            <div className={AddCss.mapPArent}>
+              {show ? (
+                <div className={AddCss.mapPArentDiv}>
+                  <div
+                    className={AddCss.AddAddressmDiv}
+                    onClick={() => setAdd(true)}
+                  >
+                    <img src={imgplus} alt="" />
+                    <p>ADD ADDRESS</p>
                   </div>
-                );
-              })}
+                  {show.map((value, key) => {
+                    return (
+                      <div key={value._id} className={AddCss.AmDiv}>
+                        <p>
+                          <b>name:</b> {value.name}
+                        </p>
+                        <p>
+                          <b>address:</b> {value.address}
+                        </p>
+                        <p>
+                          <b>pin Code:</b> {value.pinCode}
+                        </p>
+                        <p>
+                          <b>Town:</b> {value.town}
+                        </p>
+                        <p>
+                          <b>State:</b> {value.state}
+                        </p>
+                        <p>
+                          <b>Country:</b> {value.country}
+                        </p>
+                        <p>
+                          <b>Phone Number:</b> {value.phoneNumber}
+                        </p>
+                        <div className={AddCss.editHoverDiv}>
+                          <p className={AddCss.editHoverEdit}>{/* Edit */}</p>
+                          <p
+                            className={AddCss.editHoverDelete}
+                            onClick={() => {
+                              setDelete(true);
+                              setDeleteID(value._id);
+                            }}
+                          >
+                            Delete
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div
+                  className={AddCss.AddAddressmDiv}
+                  onClick={() => setAdd(true)}
+                >
+                  <img src={imgplus} alt="" />
+                  <p>ADD ADDRESS</p>
+                </div>
+              )}
             </div>
+          </div>
+          {showAdd ? <AddAddress state={setAdd} /> : ""}
+          {showDelete ? (
+            <DeleteAddress DeleteID={showDeleteID} state={set} />
           ) : (
-            <div className={AddCss.AddAddressmDiv} onClick={() => setAdd(true)}>
-              <img src={imgplus} alt="" />
-              <p>ADD ADDRESS</p>
-            </div>
+            ""
           )}
         </div>
-      </div>
-      {showAdd ? <AddAddress state={setAdd} /> : ""}
-      {showDelete ? <DeleteAddress DeleteID={showDeleteID} state={set} /> : ""}
-    </div>
+      )}
+    </>
   );
 }
