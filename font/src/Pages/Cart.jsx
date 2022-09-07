579c7e18a6a92dd02277627c50c1c6b4c3b96505
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-07 16:32:23 +0530

    Update Cart.jsx

--- font/src/Pages/Cart.jsx
+++ font/src/Pages/Cart.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CartCss from "./Css/Cart.module.css";
-import SCCss from "./Css/ShopCart.module.css";
 import empty_cart from "./../Img/wishes.svg";
 import { Link } from "react-router-dom";
 import CloseIcon from "@mui/icons-material/Close";
