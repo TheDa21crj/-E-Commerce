afab23beacf63fbf01a55903db9ffa4cf082987d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-11 20:26:09 +0530

    Update Profile.jsx

--- font/src/Components/Account/Mobile/Profile.jsx
+++ font/src/Components/Account/Mobile/Profile.jsx
@@ -6,108 +6,117 @@ import ProfileCss from "./../CSS/Profile.module.css";
 
 export default function Profile(props) {
   return (
-    <div className={Common.mdiv} id={ProfileCssMobo.mdiv}>
-      <div>
-        <KeyboardBackspaceIcon
-          fontSize="large"
-          onClick={() => props.state("")}
-        />
-      </div>
-      <p className={Common.MyPTag}>Profile</p>
-      <div className={ProfileCss.EmailDiv}>
-        <p>Email ID:</p>
-        <input
-          type="email"
-          className={ProfileCss.impEmail}
-          name=""
-          id=""
-          readonly="readonly"
-          disabled="disabled"
-          value={props.email}
-        />
-      </div>
-      <div className={ProfileCss.FormDiv}>
-        <p id={ProfileCss.GeneralI}>General Information</p>
-        <form action="" method="POST" className={ProfileCss.FormTag}>
-          <div className={ProfileCss.LeftDiv}>
-            <div className={ProfileCss.LabelDivTag1}>
-              <label htmlFor="firstName" className={ProfileCss.LabelTag1}>
-                First Name:{" "}
-              </label>
-              <input
-                type="text"
-                name="firstName"
-                id=""
-                value={props.firstName}
-                className={ProfileCss.NameInp}
-              />
-            </div>
-            <div className={ProfileCss.LabelDivTag2}>
-              <label htmlFor="lastName">Last Name: </label>
-              <input
-                type="text"
-                name="lastName"
-                id=""
-                value={props.lastName}
-                className={ProfileCss.NameInp}
-              />
-            </div>
+    <>
+      {a ? (
+        <div className={Common.mdiv} id={ProfileCssMobo.mdiv}>
+          <div>
+            <KeyboardBackspaceIcon
+              fontSize="large"
+              onClick={() => props.state("")}
+            />
           </div>
-          <div className={ProfileCss.RightDiv}>
-            <div className={ProfileCss.Gender}>
-              <p>Gender</p>
-              <div className={ProfileCss.GenderTDiv}>
-                <div>
-                  {props.gender == "Male" ? (
-                    <input type="radio" name="" id="" checked />
-                  ) : (
-                    <input type="radio" name="" id="" />
-                  )}
-                  <label htmlFor="Male" className={ProfileCss.MaleLabel}>
-                    Male
+          <p className={Common.MyPTag}>Profile</p>
+          <div className={ProfileCss.EmailDiv}>
+            <p>Email ID:</p>
+            <input
+              type="email"
+              className={ProfileCss.impEmail}
+              name=""
+              id=""
+              readonly="readonly"
+              disabled="disabled"
+              value={props.email}
+            />
+          </div>
+          <div className={ProfileCss.FormDiv}>
+            <p id={ProfileCss.GeneralI}>General Information</p>
+            <form action="" method="POST" className={ProfileCss.FormTag}>
+              <div className={ProfileCss.LeftDiv}>
+                <div className={ProfileCss.LabelDivTag1}>
+                  <label htmlFor="firstName" className={ProfileCss.LabelTag1}>
+                    First Name:{" "}
                   </label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id=""
+                    value={props.firstName}
+                    className={ProfileCss.NameInp}
+                  />
                 </div>
-                <div>
-                  {props.gender == "Female" ? (
-                    <input type="radio" name="" id="" checked />
-                  ) : (
-                    <input type="radio" name="" id="" />
-                  )}
-                  <label htmlFor="Female" className={ProfileCss.FemaleLabel}>
-                    Female
-                  </label>
-                </div>
-                <div>
-                  {props.gender == "Other" ? (
-                    <input type="radio" name="" id="" checked />
-                  ) : (
-                    <input type="radio" name="" id="" />
-                  )}
-                  <label htmlFor="Other" className={ProfileCss.OtherLabel}>
-                    Other
-                  </label>
+                <div className={ProfileCss.LabelDivTag2}>
+                  <label htmlFor="lastName">Last Name: </label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id=""
+                    value={props.lastName}
+                    className={ProfileCss.NameInp}
+                  />
                 </div>
               </div>
-            </div>
-            <div>
-              <label
-                className={ProfileCss.PhoneNumberLabel}
-                htmlFor="PhoneNumber"
-              >
-                Mobile Number:{" "}
-              </label>
-              <br />
-              <input
-                type="number"
-                name=""
-                id=""
-                value={props.phone}
-                className={ProfileCss.PhoneNumberInp}
-              />
-            </div>
+              <div className={ProfileCss.RightDiv}>
+                <div className={ProfileCss.Gender}>
+                  <p>Gender</p>
+                  <div className={ProfileCss.GenderTDiv}>
+                    <div>
+                      {props.gender == "Male" ? (
+                        <input type="radio" name="" id="" checked />
+                      ) : (
+                        <input type="radio" name="" id="" />
+                      )}
+                      <label htmlFor="Male" className={ProfileCss.MaleLabel}>
+                        Male
+                      </label>
+                    </div>
+                    <div>
+                      {props.gender == "Female" ? (
+                        <input type="radio" name="" id="" checked />
+                      ) : (
+                        <input type="radio" name="" id="" />
+                      )}
+                      <label
+                        htmlFor="Female"
+                        className={ProfileCss.FemaleLabel}
+                      >
+                        Female
+                      </label>
+                    </div>
+                    <div>
+                      {props.gender == "Other" ? (
+                        <input type="radio" name="" id="" checked />
+                      ) : (
+                        <input type="radio" name="" id="" />
+                      )}
+                      <label htmlFor="Other" className={ProfileCss.OtherLabel}>
+                        Other
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <label
+                    className={ProfileCss.PhoneNumberLabel}
+                    htmlFor="PhoneNumber"
+                  >
+                    Mobile Number:{" "}
+                  </label>
+                  <br />
+                  <input
+                    type="number"
+                    name=""
+                    id=""
+                    value={props.phone}
+                    className={ProfileCss.PhoneNumberInp}
+                  />
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
