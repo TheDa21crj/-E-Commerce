2b833e7076456c55e5e3565c8a8e7f15d05ff2b4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-11 20:49:38 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/Mobile/MyAddress.jsx
+++ font/src/Components/Account/Mobile/MyAddress.jsx
@@ -18,26 +18,59 @@ export default function MyAddress(props) {
     seeAddress();
   }, []);
 
+  useEffect(() => {
+    seeAddress();
+  }, [showAdd]);
+
+  const seeAddress = async () => {
+    try {
+      const res = await fetch("/api/Address", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (data.errors) {
+        return;
+      }
+      if (data) {
+        if (data.message === "zero") {
+          setload(false);
+          return;
+        } else {
+          setload(false);
+          set(data.message);
+          dispatch({ address: data.message });
+        }
+      }
+    } catch (error) {
+      return;
+    }
+  };
+
   return (
     <>
-      {/* {showload ? (
+      {showload ? (
         <div className={ProfileCss.LadingDiv}>
           <img src={Loading} alt="" />
         </div>
-      ) : ( */}
-      <div className={Common.mdiv}>
-        <div>
-          <KeyboardBackspaceIcon
-            fontSize="large"
-            onClick={() => props.state("")}
-          />
+      ) : (
+        <div className={Common.mdiv}>
+          <div>
+            <KeyboardBackspaceIcon
+              fontSize="large"
+              onClick={() => props.state("")}
+            />
+          </div>
+          <p className={Common.MyPTag}>My Address</p>
+          <div className={AddCss.AddressRowDiv}>
+            <div className={AddCss.mapPArent}>{}</div>
+          </div>
         </div>
-        <p className={Common.MyPTag}>My Address</p>
-        <div className={AddCss.AddressRowDiv}>
-          <div className={AddCss.mapPArent}>{}</div>
-        </div>
-      </div>
-      {/* )} */}
+      )}
     </>
   );
 }
