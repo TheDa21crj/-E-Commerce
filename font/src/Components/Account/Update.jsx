4cd6d42898aad5d148520471e9d83ddd94278d34
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-11 16:50:38 +0530

    Update Update.jsx

--- font/src/Components/Account/Update.jsx
+++ font/src/Components/Account/Update.jsx
@@ -57,12 +57,11 @@ export default function Update(props) {
             onChange={DataInp}
             placeholder="Last Name"
           />
-          <select name="gender" className={AddCss.impTag}>
+          <select name="gender" className={AddCss.impTag} onChange={changeSort}>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
             <option value="Other">Other</option>
           </select>
-
           <input
             type="number"
             name="PhoneNumber"
