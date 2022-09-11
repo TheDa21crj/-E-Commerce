2e4838bef06e89563ea02056cbb101889bf5a819
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-11 16:20:37 +0530

    Update Update.jsx

--- font/src/Components/Account/Update.jsx
+++ font/src/Components/Account/Update.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import AddCss from "./CSS/Add.module.css";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function Update() {
-  return <div>Update</div>;
+export default function Update(props) {
+  return (
+    <div className={AddCss.mDiv}>
+      <div className={AddCss.ConDiv}>
+        Update
+        <br />
+        <CloseIcon
+          onClick={() => props.state(false)}
+          fontSize="large"
+          className={AddCss.CloseIcon}
+        />
+      </div>
+    </div>
+  );
 }
