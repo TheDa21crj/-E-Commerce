6a1db70a41508133897f157e0370148f08aa07d3
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-11 20:33:36 +0530

    Update MyAddress.jsx

--- font/src/Components/Account/Mobile/MyAddress.jsx
+++ font/src/Components/Account/Mobile/MyAddress.jsx
@@ -12,7 +12,7 @@ export default function MyAddress(props) {
 
   return (
     <>
-      {a ? (
+      {showload ? (
         <div className={ProfileCss.LadingDiv}>
           <img src={Loading} alt="" />
         </div>
