9026101c325d9ec94bb1c081b7651d314a7dedec
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-11 16:30:33 +0530

    Update Update.jsx

--- font/src/Components/Account/Update.jsx
+++ font/src/Components/Account/Update.jsx
@@ -13,6 +13,63 @@ export default function Update(props) {
           fontSize="large"
           className={AddCss.CloseIcon}
         />
+        <div className={AddCss.inpDiv}>
+          <input
+            type="text"
+            name="name"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="Name"
+          />
+          <input
+            type="number"
+            name="pinCode"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="Pin Code"
+          />
+          <input
+            type="text"
+            name="address"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="Address"
+          />
+          <input
+            type="text"
+            name="town"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="Town"
+          />
+          <input
+            type="text"
+            name="state"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="State"
+          />
+          <input
+            type="text"
+            name="country"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="Country"
+          />
+          <input
+            type="text"
+            name="phoneNumber"
+            className={AddCss.impTag}
+            onChange={DataInp}
+            placeholder="Phone Number"
+          />
+          <button
+            //    onClick={PostData}
+            className={AddCss.LoginBtn}
+          >
+            ADD
+          </button>
+        </div>
       </div>
     </div>
   );
