b321f8b4f1bb445a7a5e409ddc0edf32ea305774
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-25 22:19:00 +0530

    Update CheckoutSuccess.jsx

--- font/src/Components/Pay/CheckoutSuccess.jsx
+++ font/src/Components/Pay/CheckoutSuccess.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function CheckoutSuccess() {
-  return <div>CheckoutSuccess</div>;
+  return (
+    <div>
+      CheckoutSuccess
+      <br />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, pariatur?
+      Sit quod ratione cumque molestias aperiam blanditiis quos labore quas,
+      accusamus deleniti. Praesentium eos aperiam incidunt ut enim obcaecati.
+      Reiciendis autem nisi ipsam eligendi quo nam doloribus porro
+      exercitationem tenetur. Placeat quos, deleniti quaerat explicabo delectus
+      dignissimos commodi culpa. Unde in voluptas iste reiciendis ullam
+      molestiae, est similique facilis delectus cum modi suscipit nemo natus!
+      Illum expedita voluptatibus praesentium blanditiis perferendis in. Aperiam
+      distinctio voluptatum saepe corrupti quas deleniti minima, facere illo
+      obcaecati assumenda. Ea, eius atque dolorum doloribus, veniam alias ad
+      cupiditate enim minus reprehenderit deleniti tempora quidem obcaecati!
+    </div>
+  );
 }
