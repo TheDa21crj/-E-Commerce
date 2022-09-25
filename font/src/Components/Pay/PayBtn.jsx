87212d29a340d61d4aa8f44e43c7866358f747ff
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-25 23:17:28 +0530

    Update PayBtn.jsx

--- font/src/Components/Pay/PayBtn.jsx
+++ font/src/Components/Pay/PayBtn.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SCCss from "./../../Pages/Css/ShopCart.module.css";
-import { Link } from "react-router-dom";
 
 export default function PayBtn(props) {
   const handleClick = async () => {
