4d58b0cdaf3a0c85f2fa95e2dbb64e07ba6df5c7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-25 23:27:09 +0530

    Update stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { check, validationResult } = require("express-validator");
 const config = require("config");
 const StripeKey = config.get("Stripe");
 const ClientURl = config.get("ClientURl");
@@ -9,26 +10,36 @@ const UserAuth = require("./../middleware/UserAuth");
 
 const stripe = require("stripe")(StripeKey);
 
-router.post("/checkout", UserAuth, async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "inr",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `${ClientURl}/checkout-success`,
-    cancel_url: `${ClientURl}/ShopCart`,
-  });
+router.post(
+  "/checkout",
+  [
+    UserAuth,
+    check("dataItems", "dataItems is Required").not().isEmpty(),
+    // check("name", "name is Required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    console.table(dataItems);
 
-  res.send({ url: session.url });
-});
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: "T-shirt",
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `${ClientURl}/checkout-success`,
+      cancel_url: `${ClientURl}/ShopCart`,
+    });
+
+    res.send({ url: session.url });
+  }
+);
 
 module.exports = router;
