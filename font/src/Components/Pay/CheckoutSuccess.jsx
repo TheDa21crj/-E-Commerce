dd7ea6d91d71ae58c1e0b20d6608ed2f2b22d923
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-26 16:28:36 +0530

    Update CheckoutSuccess.jsx

--- font/src/Components/Pay/CheckoutSuccess.jsx
+++ font/src/Components/Pay/CheckoutSuccess.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function CheckoutSuccess() {
+  useEffect(() => {
+    deleteWish();
+  }, []);
+
+  const deleteWish = async (deleteID) => {
+    let id = deleteID;
+    if (id === "") {
+      return;
+    } else {
+      try {
+        const res = await fetch("/api/Shoping/delete/product/all", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        let r = await res.json();
+
+        console.log(r);
+        // seeList();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return <div>CheckoutSuccess</div>;
 }
