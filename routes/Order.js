630d1f1d9b1ad1355fe423f0ffa473d5cc07d604
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-26 21:37:49 +0530

    Update Order.js

--- routes/Order.js
+++ routes/Order.js
@@ -5,3 +5,21 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const UserAuth = require("./../middleware/UserAuth");
+
+// Private || See Shoping || api/Shoping
+router.get("/", UserAuth, async (req, res) => {
+  let userID = req.userId;
+  try {
+    let userCheck = await Shoping.findOne({ user: userID });
+    if (userCheck) {
+      return res.status(200).json({ message: userCheck.List });
+    } else {
+      return res.status(200).json({ message: "zero" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+});
+
+module.exports = router;
