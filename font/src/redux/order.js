34c054191136c915a6ba4fb9c465307016f05040
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-27 06:21:54 +0530

    Update order.js

---
+++ font/src/redux/order.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const OrderSlice = createSlice({
+  name: "order",
+  initialState: {
+    orderList: [],
+  },
+  reducers: {
+    addselling: (state, action) => {
+      state.orderList = action.payload.orderList;
+    },
+  },
+});
+
+export const { addOrder } = OrderSlice.actions;
+export default OrderSlice.reducer;
