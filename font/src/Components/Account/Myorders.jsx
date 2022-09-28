fd889cd5b7678e357dd626127c9b94d0da7e8afb
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-28 15:42:28 +0530

    Update Myorders.jsx

--- font/src/Components/Account/Myorders.jsx
+++ font/src/Components/Account/Myorders.jsx
@@ -3,12 +3,37 @@ import ProfileCss from "./CSS/Profile.module.css";
 import OrderCss from "./CSS/Orders.module.css";
 
 export default function Myorders(props) {
+  const [showData, setData] = useState([]);
+
+  useEffect(() => {
+    seeOrders();
+  }, []);
+
+  const seeOrders = async (e) => {
+    try {
+      const res = await fetch("/api/Order", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (data.errors) {
+        return;
+      }
+      if (data) {
+        setData(data);
+      }
+    } catch (error) {
+      return;
+    }
+  };
   return (
     <div className={ProfileCss.mdiv}>
       <h1 className={ProfileCss.h1}>My Orders</h1>
-      {/* <div className={OrderCss.pTagMain}> */}
       <p className={OrderCss.pTagMain}>No Orders</p>
-      {/* </div> */}
     </div>
   );
 }
