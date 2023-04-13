c91b8671412af3cf5df78f9a20e880dd2ab6da91
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 23:08:57 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,24 +5,24 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 
-const Auth = async(req, res, next) => {
-    try {
-        const token = req.cookies.jwtTokenAuth;
-        const vToken = jwt.verify(token, config.get("jwtTokenAuth"));
-        const dataUser = await User.findOne({
-            _id: vToken._id,
-            "tokens.token": token,
-        });
-        if (!dataUser) {
-            throw new Error("Couldn't find");
-        }
-        req.token = token;
-        req.dataUser = dataUser;
-        req.userId = dataUser._id;
-
-        next();
-    } catch (error) {
-        res.status(401).json({ errors: error });
+const Auth = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwtTokenAuth;
+    const vToken = jwt.verify(token, config.get("jwtTokenAuth"));
+    const dataUser = await User.findOne({
+      _id: vToken._id,
+      "tokens.token": token,
+    });
+    if (!dataUser) {
+      throw new Error("Couldn't find");
     }
+    req.token = token;
+    req.dataUser = dataUser;
+    req.userId = dataUser._id;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ errors: error });
+  }
 };
 module.exports = Auth;
