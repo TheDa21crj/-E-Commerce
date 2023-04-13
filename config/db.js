ef8aba7611629bcebf3ea38a0977962b919681d8
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 23:08:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURL");
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-        });
-        console.log("DataBase Connected");
-    } catch (error) {
-        console.log(error);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+    console.log("DataBase Connected");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectDB;
